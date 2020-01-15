b01caa47db10093b80f902b97e706138b22b7f32
Devin Buhl <devin.kray@gmail.com>
2020-01-15 16:31:26 -0500

    add minio

--- secrets/secrets.sh
+++ secrets/secrets.sh
@@ -54,10 +54,10 @@ kseal() {
 # Helm Secrets
 #
 
-# kseal "${REPO_ROOT}/deployments/velero/velero/velero-helm-values.txt"
-# kseal "${REPO_ROOT}/deployments/default/minio/minio-helm-values.txt"
-# kseal "${REPO_ROOT}/deployments/default/nzbget/nzbget-helm-values.txt"
+kseal "${REPO_ROOT}/deployments/default/minio/minio-helm-values.txt"
 
+# kseal "${REPO_ROOT}/deployments/velero/velero/velero-helm-values.txt"
+# kseal "${REPO_ROOT}/deployments/default/nzbget/nzbget-helm-values.txt"
 # kseal "${REPO_ROOT}/deployments/default/radarr/radarr-helm-values.txt"
 # kseal "${REPO_ROOT}/deployments/default/sonarr/sonarr-helm-values.txt"
 # kseal "${REPO_ROOT}/deployments/default/qbittorrent/qbittorrent-helm-values.txt"
