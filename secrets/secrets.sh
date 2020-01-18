940b16d7bc8352df4fbaa18639f3b6ddc5787368
Devin Buhl <devin.kray@gmail.com>
2020-01-18 10:24:55 -0500

    add velero
    Former-commit-id: d86b3e667f560ba074ffca4183f8e2072bd69601

--- secrets/secrets.sh
+++ secrets/secrets.sh
@@ -55,8 +55,8 @@ kseal() {
 #
 
 kseal "${REPO_ROOT}/deployments/default/minio/minio-helm-values.txt"
+kseal "${REPO_ROOT}/deployments/velero/velero/velero-helm-values.txt"
 
-# kseal "${REPO_ROOT}/deployments/velero/velero/velero-helm-values.txt"
 # kseal "${REPO_ROOT}/deployments/default/nzbget/nzbget-helm-values.txt"
 # kseal "${REPO_ROOT}/deployments/default/radarr/radarr-helm-values.txt"
 # kseal "${REPO_ROOT}/deployments/default/sonarr/sonarr-helm-values.txt"
