f9739f673a83a104ea7a5a201533b9bf5b36fe6a
Devin Buhl <devin.kray@gmail.com>
2020-08-08 14:11:59 -0400

    ass taskfile

--- secrets/sample-env.sh
+++ secrets/sample-env.sh
@@ -4,3 +4,5 @@ export REPO_ROOT
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 sed 's/=.*/=""/' $REPO_ROOT/secrets/.secrets.env > $REPO_ROOT/secrets/.secrets.sample.env
+
+cat "${REPO_ROOT}/secrets/.secrets.sample.env"
