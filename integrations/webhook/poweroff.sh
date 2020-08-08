58f9f9aa517318a43fa311d3893c58d321c63485
Devin Buhl <devin.kray@gmail.com>
2020-08-08 15:26:35 -0400

    update webhook

--- integrations/webhook/poweroff.sh
+++ integrations/webhook/poweroff.sh
@@ -2,4 +2,4 @@
 
 # webhook -hooks integrations/webhook/hooks.json -debug -verbose
 export ANSIBLE_CONFIG=./ansible.cfg
-ansible-playbook -i hosts.yaml zpm-depleted.yaml
+ansible-playbook -i hosts.yaml playbooks/poweroff.yaml
