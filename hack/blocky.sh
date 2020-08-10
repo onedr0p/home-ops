b51f32af10cd7afe6724fc21b17e9d25652272af
Devin Buhl <devin.kray@gmail.com>
2020-08-10 10:38:24 -0400

    add blocky hack

---
+++ hack/blocky.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ACTION=$1
+DURATION=$2
+
+NAMESPACE="kube-system"
+BLOCKY_PODS=$(kubectl get pods -n "${NAMESPACE}" -o=jsonpath="{range .items[*]}{.metadata.name} " -l app.kubernetes.io/name=blocky)
+
+for pod in $BLOCKY_PODS; do
+    case "${ACTION}" in
+        status)
+            kubectl -n "${NAMESPACE}" exec -it "${pod}" -- /app/blocky blocking status;
+        ;;
+        enable)
+            kubectl -n "${NAMESPACE}" exec -it "${pod}" -- /app/blocky blocking enable;
+        ;;
+        disable)
+            if [ -z "${DURATION}" ]; then
+                kubectl -n "${NAMESPACE}" exec -it "${pod}" -- /app/blocky blocking disable
+            else
+                kubectl -n "${NAMESPACE}" exec -it "${pod}" -- /app/blocky blocking disable --duration "${DURATION}";
+            fi
+        ;;
+    esac
+done
