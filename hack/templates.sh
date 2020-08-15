887b21e969a537b071fc8e160b63fd317d16dc57
Devin Buhl <devin.kray@gmail.com>
2020-08-15 17:56:28 -0400

    add dotenv2setenv hack

--- hack/templates.sh
+++ hack/templates.sh
@@ -23,23 +23,9 @@ else
   . "${REPO_ROOT}/.cluster-secrets.env"
 fi
 
-# echo "~~~~~~~~~~~~~~~~~~~~~~"
-# echo ">>> ${TEST_SECRET1} <<<"
-# echo "~~~~~~~~~~~~~~~~~~~~~~"
-
-
-# export TEST_TEST="blah"
-# echo "Will this subst, ples? \${TEST_TEST}" | envsubst
-
-# echo "Will this subst1? \${TEST_SECRET1}" | envsubst
-# echo "Will this subst2? \${TEST_SECRET2}" | envsubst
-# echo "Will this subst3? \${TEST_SECRET3}" | envsubst
-
-# printenv
-
 for file in "${CLUSTER_ROOT}"/_templates/*.tpl
 do
-  if output=$(envsubst -no-unset -no-empty -fail-fast < "$file"); then
+  if output=$(envsubst < "$file"); then
     printf '%s' "$output" | kubectl apply -f -
   fi
 done
