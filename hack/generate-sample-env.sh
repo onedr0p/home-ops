c95e964db2056f16b62dea30fe4fc13510a00146
Devin Buhl <devin.kray@gmail.com>
2020-08-20 08:38:09 -0400

    more linting and pre-commit changes
    Former-commit-id: da5be597f941d9d66a35cd5ce702c74e9e35c592 [formerly 12b6c5d17ebf7a581ded5b183a798458d7254806] [formerly ee0f351d4b2fb87a2a16419a79c135729f447e9e [formerly 196b90fb82e8620e0a6fd714de4ef785289eeaea]]
    Former-commit-id: c67f72854380ecb43238f3b267a7189b3920af10 [formerly dd8c70bbfeea32db38b5d24bb8fbe896b412471c]
    Former-commit-id: 3d6408f3fda2b49da5303f484281e09164479403

--- hack/generate-sample-env.sh
+++ hack/generate-sample-env.sh
@@ -5,5 +5,5 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${__dir}/environment.sh"
 
-sed 's/=.*/=""/' "${REPO_ROOT}/.cluster-secrets.env" > "${REPO_ROOT}/.cluster-secrets.sample.env"
+sed 's/=.*/=""/' "${REPO_ROOT}/.cluster-secrets.env" >"${REPO_ROOT}/.cluster-secrets.sample.env"
 cat "${REPO_ROOT}/.cluster-secrets.sample.env"
