07345ed4d7646adc2cd6adc622e77ece99a47eca
Devin Buhl <devin.kray@gmail.com>
2020-11-25 09:11:32 -0500

    update renovatebot parsing helm repos
    Former-commit-id: 6627f3ef1d05bdd445b20b0f22187f82cc905c35 [formerly c8e4dc57ee6a2054a7c673547cb528edd53ffde7] [formerly f6b1093b83d820720084fc0ca865d49243f9e069 [formerly 3643449db80965d670cd8b1a8fe6c3ec03a6a7ef]] [formerly 15103967f2f7663f1cd993291a306ab4da105da7 [formerly 5b1fcd7b07a0733f22f8f20c4bc19fade3d348f6] [formerly cbd4bcb765562f672493afd3090b39ae610480e3 [formerly ce0d2142c455c7146a9babb554be6916b1520666]]] [formerly 249aca0bbcadc802de71ec7714cebab051fb4607 [formerly 7c4506a495d625fb721b550a17b23e76b154183a] [formerly b58334a7a1a1cb51571b4b6ba2842c2cea17a83c [formerly c4e07264507aa654e759c44d48e097f16aa095c6]] [formerly 04901a61da0e172d8ec9cd37467adb946ef2b5fc [formerly 7f382cbca6b280403d3d67c8803777d2dd27120d] [formerly ce0d2142c455c7146a9babb554be6916b1520666 [formerly 48f22c06f6f196a1188cc1b14074fbf4159f2f30]]]]
    Former-commit-id: 7923323bac750736e9192348037cf4b5c350c2ea [formerly 663b5a734875079d2c1334038eb92bd38c24e372] [formerly 098e6247bf639e61c295ac4f39d840da30531b6d [formerly 2c8b2c59a9f021a907e41453f3b4c63971f59523]] [formerly d17eb73df029e2a98d936b996d26e3022146b3ff [formerly 051e85477e9196020f1c0570fb6b3b8ff763f6ef] [formerly 93f32e7be0568cb2e8f9bec361e49b0cad4002d3 [formerly 4c3e7b0baa9423bb78e2a7d002cdbae1c70eef4b]]]
    Former-commit-id: b30a2136820c2b143bc5540e059d10fa2da8cf2c [formerly 143dd845619769223513631a9940dab821fa45f1] [formerly 93f32e7be0568cb2e8f9bec361e49b0cad4002d3]
    Former-commit-id: 2d4a33ff3c4294dba8019a945f84fde28a39d489 [formerly 9d845cf34d1b81f4c0beb26d8f7ea7e8a80d741b]
    Former-commit-id: 74b5832572fb7bc78522bc863ac23d69e0b56480

--- hack/renovatebot-charts-annotation.sh
+++ hack/renovatebot-charts-annotation.sh
@@ -5,35 +5,30 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${__dir}/environment.sh"
 
-export repository_files="${CLUSTER_ROOT}/flux-system/helm-chart-repositories"
+export helm_repositories="${CLUSTER_ROOT}/flux-system/helm-chart-repositories"
 
-# loop thru and get all repository name and URLs
-for file in "${repository_files}"/*.yaml; do
-    chart_name=$(yq r "$file" metadata.name)
-    chart_url=$(yq r "$file" spec.url)
+for helm_release in "${CLUSTER_ROOT}"/**/*.yaml; do
+    # ignore flux-system namespace
+    # ignore wrong apiVersion
+    # ignore non HelmReleases
+    if [[ "${helm_release}" =~ "flux-system"
+        || $(yq r "${helm_release}" apiVersion) != "helm.toolkit.fluxcd.io/v2beta1"
+        || $(yq r "${helm_release}" kind) != "HelmRelease" ]]; then
+        continue
+    fi
 
-    echo "$chart_name : $chart_url"
+    for helm_repository in "${helm_repositories}"/*.yaml; do
+        chart_name=$(yq r "${helm_repository}" metadata.name)
+        chart_url=$(yq r "${helm_repository}" spec.url)
 
-    # loop thru all namespaces and update HelmReleases
-    for helm_release in "${CLUSTER_ROOT}"/**/*.yaml; do
-        # ignore flux-system namespace
-        # ignore wrong apiVersion
-        # ignore non HelmReleases
-        # ignore files that are not in $chart_name
-        if [[ "${helm_release}" =~ "flux-system"
-            || $(yq r "${helm_release}" apiVersion) != "helm.toolkit.fluxcd.io/v2beta1"
-            || $(yq r "${helm_release}" kind) != "HelmRelease"
-            || $(yq r "${helm_release}" spec.chart.spec.sourceRef.name) != "${chart_name}" ]]; then
-            continue
+        # only helmreleases where helm_release is related to chart_url
+        if [[ $(yq r "${helm_release}" spec.chart.spec.sourceRef.name) == "${chart_name}" ]]; then
+            # remove "renovatebot.helm.repository" line
+            sed -i "/renovate: registryUrl=/d" "${helm_release}"
+            # add "renovatebot.helm.repository" line
+            sed -i "/.*chart: .*/i \ \ \ \ \ \ # renovate: registryUrl=${chart_url}" "${helm_release}"
+            echo "Annotated $(basename "${helm_release%.*}") with ${chart_name} for renovatebot..."
+            break
         fi
-
-        # remove "renovatebot.helm.repository" line
-        sed -i "/renovatebot.helm.repository/d" "${helm_release}"
-
-        # add "renovatebot.helm.repository" line
-        sed -i "/.*chart: .*/i \ \ \ \ \ \ # renovatebot.helm.repository: ${chart_url}" "${helm_release}"
-
-        # awk '!found && /.*chart:\s[A-Za-z0-9]+$/ { print "      # renovatebot.helm.repository: $chart_url"; found=1 } 1' "${helm_release}"
-        echo "$helm_release"
     done
 done
