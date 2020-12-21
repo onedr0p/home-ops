17d6e6a82f941fea3eebb73a15b38434f7889164
ᗪєνιη ᗷυнʟ <onedr0p@users.noreply.github.com>
2020-12-21 10:22:02 -0500

    migrate to yq v4
    Former-commit-id: b987bf6a14647eeb4a11ba1ffae3d30f3886892e [formerly 0e3178485173a37eaab5fafc6ff78da372368ef4] [formerly bf8e34ec1b5cfa1ec8461757b89ca41f8ed37b33 [formerly 8af3c5721396e8776fe8d5bb3e5ad2c132b8040c]] [formerly c7575dc0b75fc762ee6092de99ddf2895c79d911 [formerly 4fc53cf981cd0b3a68c631ef54d62da268ace9d0] [formerly 4c5e2f0053eb1ab743ae55672ec1309b9298335d]] [formerly d666b8b06af9f818a5f6e92fecc42de0ce732dce [formerly 86331bedfcc4b0bd4e2aff6fcbee0fafd9e87a96] [formerly eb50eeee97d96c793f0fae18f9e891145ce5d4bb [formerly f73c3e1d77b3258c9f100e24dfe741cd1c051f72]] [formerly eef96e23692b7ae3ed54381e88de1e87c0222e7a [formerly ba9e1fb4ac4fa8f11b791a57450c72ce1161ba03] [formerly 4c5e2f0053eb1ab743ae55672ec1309b9298335d [formerly fb9ef4bf29d1cacbc2c7fb657a50e85f13e6c2b3]]]]
    Former-commit-id: b6d51b4e79c83ee8a20cdb172dd429aef276b4ec [formerly ba818ade5658657cd49fa35ff5d10b52f0402575] [formerly 6ac6a5e5bf0ee998e818cab27027e31d65539fe4 [formerly d3386fabf5954c96b05344b8ae51ee6d8cb562d5]] [formerly 48033e73fcf1640c7ab15013fca673e19fb9b1a5 [formerly 995bb8af21eb513ab720fbd462c2d2d3a6ae7a77] [formerly ae174626ec0d8ec2d0b4456ad332fe4a71890f38 [formerly b8bfbb3889898afb2fd71f363eded825b70d1ca3]]]
    Former-commit-id: f536fbec374040ebcc05c31fa9aab825e4bdc108 [formerly b992dc3eb5645ff648c7f0968207f3d072e983d0] [formerly ae174626ec0d8ec2d0b4456ad332fe4a71890f38]
    Former-commit-id: cf87c46b01fd0965f15543eed60bf54f9fbeb3f3 [formerly d9c9117f1eba9f885570af227a5684072c071a81]
    Former-commit-id: b71ebe6a27e86d018e93e3f5a40796c15a761618

--- hack/create-secrets.sh
+++ hack/create-secrets.sh
@@ -156,7 +156,7 @@ echo "---" >>"${GENERATED_SECRETS}"
 sed -i '/^[[:space:]]*$/d' "${GENERATED_SECRETS}"
 
 # Validate Yaml
-if ! yq validate "${GENERATED_SECRETS}" >/dev/null 2>&1; then
+if ! yq eval "${GENERATED_SECRETS}" >/dev/null 2>&1; then
     echo "Errors in YAML"
     exit 1
 fi
