43544171d0ca7c5f73fba6fcd0608b330bac680e
Devin Buhl <devin.kray@gmail.com>
2021-01-19 12:20:15 -0500

    fix: update prom crd updater

--- hack/update-kube-prom-op-crds.sh
+++ hack/update-kube-prom-op-crds.sh
@@ -5,15 +5,17 @@ VERSION=$1
 [ -z "${VERSION}" ] && echo "Pass prometheus-operator version as first comandline argument" && exit 1
 
 FILES=(
-  "crd-alertmanagerconfigs.yaml"
-  "crd-alertmanagers.yaml"
-  "crd-podmonitors.yaml"
-  "crd-probes.yaml"
-  "crd-prometheuses.yaml"
-  "crd-prometheusrules.yaml"
-  "crd-servicemonitors.yaml"
-  "crd-thanosrulers.yaml"
+  "monitoring.coreos.com_alertmanagerconfigs.yaml"
+  "monitoring.coreos.com_alertmanagers.yaml"
+  "monitoring.coreos.com_podmonitors.yaml"
+  "monitoring.coreos.com_probes.yaml"
+  "monitoring.coreos.com_prometheuses.yaml"
+  "monitoring.coreos.com_prometheusrules.yaml"
+  "monitoring.coreos.com_servicemonitors.yaml"
+  "monitoring.coreos.com_thanosrulers.yaml"
 )
+
+# https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.45.0/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
 
 for file in "${FILES[@]}" ; do
     kubectl apply -f "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${VERSION}/example/prometheus-operator-crd/${file}"
