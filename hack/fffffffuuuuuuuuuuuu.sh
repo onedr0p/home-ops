f95e66f46bd115b5514adc3165f934b11a7b078c
Devin Buhl <devin.kray@gmail.com>
2021-04-09 10:48:39 -0400

    fix: update docs and add kustomization

--- hack/fffffffuuuuuuuuuuuu.sh
+++ hack/fffffffuuuuuuuuuuuu.sh
@@ -7,9 +7,3 @@ for NS in $(kubectl get ns 2>/dev/null | grep Terminating | cut -f1 -d ' '); do
     sed -i '' "s/\"kubernetes\"//g" /tmp/$NS.json
     kubectl replace --raw "/api/v1/namespaces/$NS/finalize" -f /tmp/$NS.json
 done
-
-# for POD in $(kubectl get runners --all-namespaces); do
-
-#   k -n actions-runner-system patch runner.actions.summerwind.dev runner-deployment-mbn6l-ts5f4 -p '{"metadata":{"finalizers":[]}}' --type=merge
-
-# done
