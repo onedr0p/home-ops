9fbd109a0c9e4df5582b8fda8788bb879d43b85b
Devin Buhl <devin.kray@gmail.com>
2021-04-10 15:54:20 -0400

    feat: add sealed secrets

--- hack/gen-helm-secrets.sh
+++ hack/gen-helm-secrets.sh
@@ -2,7 +2,7 @@
 set -eu
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-CLUSTER_ROOT="${PROJECT_ROOT}/cluster"
+CLUSTER_ROOT="${PROJECT_ROOT}/cluster/namespaces"
 PUB_CERT="${PROJECT_ROOT}/sealed-secrets-public-cert.pem"
 SECRETS_ENV="${PROJECT_ROOT}/.cluster-secrets.env"
 
