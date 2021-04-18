374a75d55032fd2a5f73ac8f583e36fcc342fc00
Devin Buhl <devin.kray@gmail.com>
2021-04-18 08:55:47 -0400

    fix: clean up the repo root

--- hack/fffffffuuuuuuuuuuuu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-for NS in $(kubectl get ns 2>/dev/null | grep Terminating | cut -f1 -d ' '); do
-    kubectl get ns $NS -o json > /tmp/$NS.json
-    sed -i '' "s/\"kubernetes\"//g" /tmp/$NS.json
-    kubectl replace --raw "/api/v1/namespaces/$NS/finalize" -f /tmp/$NS.json
-done
