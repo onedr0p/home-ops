87cfe1e6b8f219c4334d016cd8e9c56a82632a2b
Devin Buhl <devin.kray@gmail.com>
2021-04-22 22:04:25 -0400

    feat: add playlist, fix for healthchecks

---
+++ hack/playlist.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+while true; do
+    figlet -f slant "media" | lolcat
+    kubecolor get po -n media; sleep 5; clear;
+    figlet -f slant "home" | lolcat
+    kubecolor get po -n home; sleep 5; clear;
+    figlet -f slant "nodes" | lolcat
+    kubecolor get nodes; sleep 5; clear;
+done
