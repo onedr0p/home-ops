109ceb9b343b311bdf6a88921f25b05effe4bf6d
Devin Buhl <devin.kray@gmail.com>
2021-10-04 12:26:01 -0400

    fix: add sc cf tf rules
    Signed-off-by: Devin Buhl <devin.kray@gmail.com>

--- provision/terraform/cloudflare/firewall_rules.tf
+++ provision/terraform/cloudflare/firewall_rules.tf
@@ -16,6 +16,40 @@ resource "cloudflare_firewall_rule" "countries" {
 }
 
 #
+# Bots
+#
+
+resource "cloudflare_filter" "bots" {
+  zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  description = "Expression to block bots determined by CF"
+  expression  = "(cf.client.bot)"
+}
+
+resource "cloudflare_firewall_rule" "bots" {
+  zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  description = "Firewall rule to block bots determined by CF"
+  filter_id   = cloudflare_filter.bots.id
+  action      = "block"
+}
+
+#
+# Block threats less than Medium
+#
+
+resource "cloudflare_filter" "threats" {
+  zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  description = "Expression to block medium threats"
+  expression  = "(cf.threat_score gt 14)"
+}
+
+resource "cloudflare_firewall_rule" "threats" {
+  zone_id     = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  description = "Firewall rule to block medium threats"
+  filter_id   = cloudflare_filter.threats.id
+  action      = "block"
+}
+
+#
 # UptimeRobot
 #
 
