8a58461f9e7e179c3cd11c834ca36b297ede9c9e
Devin Buhl <devin@buhl.casa>
2021-11-20 10:21:48 -0500

    fix: use regex in uptimerobot script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- cluster/apps/monitoring/uptimerobot-heartbeat/uptimerobot-heartbeat.sh
+++ cluster/apps/monitoring/uptimerobot-heartbeat/uptimerobot-heartbeat.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -o nounset
-set -o errexit
-
-if [ -z "$UPTIMEROBOT_HEARTBEAT_URL" ]; then
+if [ -z "${UPTIMEROBOT_HEARTBEAT_URL}" ]; then
     printf "%s - Yikes - Missing UPTIMEROBOT_HEARTBEAT_URL environment variable" "$(date -u)"
     exit 0
 fi
 
-status_code=$(curl --connect-timeout 10 --max-time 30 -I -s -o /dev/null -w '%{http_code}' "$UPTIMEROBOT_HEARTBEAT_URL")
-if [ "${status_code}" != "200" ]; then
-    printf "%s - Yikes - Heartbeat request failed, http code: %s" "$(date -u)" "$status_code"
+status_code=$(curl --connect-timeout 10 --max-time 30 -I -s -o /dev/null -w '%{http_code}' "${UPTIMEROBOT_HEARTBEAT_URL}")
+if [[ ! ${status_code} =~ ^[2|3][0-9]{2}$ ]]; then
+    printf "%s - Yikes - Heartbeat request failed, http code: %s" "$(date -u)" "${status_code}"
     exit 0
 fi
 
 printf "%s - Success - Heartbeat request received and processed successfully" "$(date -u)"
+exit 0
