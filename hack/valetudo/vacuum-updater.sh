112493847ac8ff3a4817c0f433e06fe17e30233f
Devin Buhl <devin@buhl.casa>
2021-12-05 11:35:21 -0500

    feat: improve valetudo hack
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- hack/valetudo/vacuum-updater.sh
+++ hack/valetudo/vacuum-updater.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 #
+# One-shot script to install/upgrade valetudo, node-exporter and promtail
+#
+
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+VACUUM_USER="root"
+VACUUM_ADDR="192.168.1.28"
+SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR"
+
+#
 # valetudo
 #
 
@@ -8,14 +17,19 @@ version="$(curl -sX GET "https://api.github.com/repos/Hypfer/Valetudo/releases/l
 echo "*** Downloading Valetudo... ***"
 curl -fsSL -o "/tmp/valetudo" \
     "https://github.com/Hypfer/Valetudo/releases/download/${version}/valetudo-armv7"
-echo "*** Copying Valetudo to the Vacuum... ***"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q \
-    /tmp/valetudo \
-    root@192.168.1.28:/mnt/data/valetudo/valetudo
 
-ssh -T root@192.168.1.28 <<'EOL'
+ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
     echo "*** Stopping Valetudo... ***"
     /etc/init/S11valetudo stop > /dev/null 2>&1
+    sleep 10
+EOL
+
+echo "*** Copying Valetudo to the Vacuum... ***"
+scp "${SSH_OPTS}" -q \
+    /tmp/valetudo \
+    "${VACUUM_USER}@${VACUUM_ADDR}:/mnt/data/valetudo/valetudo"
+
+ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
     echo "*** Remove Valetudo... ***"
     rm /usr/local/bin/valetudo
     echo "*** Move Valetudo... ***"
@@ -24,37 +38,6 @@ ssh -T root@192.168.1.28 <<'EOL'
     chmod +x /usr/local/bin/valetudo
     echo "*** Starting Valetudo... ***"
     /etc/init/S11valetudo start > /dev/null 2>&1
-EOL
-
-#
-# promtail
-#
-
-version="$(curl -sX GET "https://api.github.com/repos/grafana/loki/releases/latest" | jq --raw-output '.tag_name')"
-echo "*** Downloading Promtail... ***"
-curl -fsSL -o "/tmp/promtail.zip" \
-    "https://github.com/grafana/loki/releases/download/${version}/promtail-linux-arm.zip"
-
-echo "*** Extracting Promtail ... ***"
-unzip -q -o /tmp/promtail.zip -d /tmp
-
-echo "*** Copying Promtail to the Vacuum... ***"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q \
-    /tmp/promtail-linux-arm \
-    root@192.168.1.28:/mnt/data/promtail/promtail
-
-echo "*** Copying Promtail init script to the Vacuum... ***"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q \
-    ./hack/valetudo/S11promtail \
-    root@192.168.1.28:/etc/init/S11promtail
-
-ssh -T root@192.168.1.28 <<'EOL'
-    echo "*** Stopping Promtail... ***"
-    /etc/init/S11promtail stop > /dev/null 2>&1
-    echo "*** Make Promtail Executable... ***"
-    chmod +x /mnt/data/promtail/promtail
-    echo "*** Starting Promtail... ***"
-    /etc/init/S11promtail start > /dev/null 2>&1
 EOL
 
 #
@@ -69,21 +52,60 @@ curl -fsSL -o "/tmp/node_exporter.tar.gz" \
 echo "*** Extracting node_exporter ... ***"
 tar zxf /tmp/node_exporter.tar.gz --strip-components=1 -C /tmp
 
+echo "*** Stopping node_exporter... ***"
+ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
+    /etc/init/S11node_exporter stop > /dev/null 2>&1
+    sleep 10
+EOL
+
 echo "*** Copying node_exporter to the Vacuum... ***"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q \
+scp "${SSH_OPTS}" -q \
     /tmp/node_exporter \
-    root@192.168.1.28:/mnt/data/node_exporter/node_exporter
+    "${VACUUM_USER}@${VACUUM_ADDR}:/mnt/data/node_exporter/node_exporter"
 
 echo "*** Copying node_exporter init script to the Vacuum... ***"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q \
-    ./hack/valetudo/S11node_exporter \
-    root@192.168.1.28:/etc/init/S11node_exporter
+scp "${SSH_OPTS}" -q \
+    "${PROJECT_DIR}/hack/valetudo/S11node_exporter" \
+    "${VACUUM_USER}@${VACUUM_ADDR}:/etc/init/S11node_exporter"
 
-ssh -T root@192.168.1.28 <<'EOL'
-    echo "*** Stopping node_exporter... ***"
-    /etc/init/S11node_exporter stop > /dev/null 2>&1
+ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
     echo "*** Make node_exporter Executable... ***"
     chmod +x /mnt/data/node_exporter/node_exporter
     echo "*** Starting node_exporter... ***"
     /etc/init/S11node_exporter start > /dev/null 2>&1
+EOL
+
+#
+# promtail
+#
+
+version="$(curl -sX GET "https://api.github.com/repos/grafana/loki/releases/latest" | jq --raw-output '.tag_name')"
+echo "*** Downloading Promtail... ***"
+curl -fsSL -o "/tmp/promtail.zip" \
+    "https://github.com/grafana/loki/releases/download/${version}/promtail-linux-arm.zip"
+
+echo "*** Extracting Promtail ... ***"
+unzip -q -o /tmp/promtail.zip -d /tmp
+
+echo "*** Stopping Promtail... ***"
+ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
+    /etc/init/S11promtail stop > /dev/null 2>&1
+    sleep 60
+EOL
+
+echo "*** Copying Promtail to the Vacuum... ***"
+scp "${SSH_OPTS}" -q \
+    /tmp/promtail-linux-arm \
+    "${VACUUM_USER}@${VACUUM_ADDR}:/mnt/data/promtail/promtail"
+
+echo "*** Copying Promtail init script to the Vacuum... ***"
+scp "${SSH_OPTS}" -q \
+    "${PROJECT_DIR}/hack/valetudo/S11promtail" \
+    "${VACUUM_USER}@${VACUUM_ADDR}:/etc/init/S11promtail"
+
+ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
+    echo "*** Make Promtail Executable... ***"
+    chmod +x /mnt/data/promtail/promtail
+    echo "*** Starting Promtail... ***"
+    /etc/init/S11promtail start > /dev/null 2>&1
 EOL
