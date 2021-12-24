b12e11af055e27bc4e3790eccc814ade384b4a5f
Devin Buhl <devin@buhl.casa>
2021-12-23 19:45:16 -0500

    chore(ci): update workflows
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ hack/image-pull-tester.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# images=()
+# image1=$(grep -oP "(?<=image:).*" cluster/apps/media/plex/helm-release.yaml | tr -d "[:blank:]")
+# image2=$(grep -oP "(?<=image:).*" ansible/storage/roles/apps.storage/templates/cadvisor/docker-compose.yml.j2 | tr -d "[:blank:]")
+# images+=( "${image1}" "${image2}" )
+
+# for t in "${images[@]}"; do
+#     if [[ -n "${t}" ]]; then
+#         echo "z ${t}"
+#     fi
+# done
+
+
+image=$(yq e '.services.*.image' cluster/apps/media/plex/helm-release.yaml)
+if [[ "${image}" == "null" ]]; then
+    repository=$(yq e '.spec.values.image.repository' cluster/apps/media/plex/helm-release.yaml)
+    tag=$(yq e '.spec.values.image.tag' cluster/apps/media/plex/helm-release.yaml)
+    image="${repository}:${tag}"
+fi
+
+docker pull $image
+echo $?
