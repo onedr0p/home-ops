1cdf5dba6666526904c242ff0dbad88dc0213471
Devin Buhl <devin@buhl.casa>
2021-12-31 21:03:39 -0500

    feat(cluster): add kasten-io namespace
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- cluster/apps/networking/secret-reflector/secret-reflector.sh
+++ cluster/apps/networking/secret-reflector/secret-reflector.sh
@@ -7,7 +7,7 @@ secrets="${SECRETS}"
 # source namespace to reflect secret from
 namespace_source="networking"
 # space delimited namespace where to reflect the secrets to
-namespace_destination="kasten-io media"
+namespace_destination="kasten-io"
 for secret in ${secrets}; do
     secret_source_content="$(kubectl get secret "${secret}" -n "${namespace_source}" -o json | jq 'del(.metadata.managedFields, .metadata.creationTimestamp, .metadata.resourceVersion, .metadata.uid)')"
     secret_source_checksum="$(echo "${secret_source_content}" | jq 'del(.metadata.namespace)' | md5sum | awk '{ print $1 }')"
