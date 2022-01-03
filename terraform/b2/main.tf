98998b327ef07a6bf7d8ab044f83d3f1ca4a4144
Devin Buhl <devin@buhl.casa>
2022-01-03 11:44:55 -0500

    feat(ansible): rework backup storage role to be more DRY and add in etcd backups
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- terraform/b2/main.tf
+++ terraform/b2/main.tf
@@ -33,6 +33,7 @@ locals {
     "opnsense-d5b252ad" = { bucket_type = "allPrivate", file_name_prefix = "", days_from_hiding_to_deleting = 1, days_from_uploading_to_hiding = null },
     "music-rcUFz3wc" = { bucket_type = "allPrivate", file_name_prefix = "", days_from_hiding_to_deleting = 1, days_from_uploading_to_hiding = null },
     "cluster-app-data-xQdC743a" = { bucket_type = "allPrivate", file_name_prefix = "", days_from_hiding_to_deleting = 1, days_from_uploading_to_hiding = null },
+    "k3s-etcd-snapshots-s0DL5a1k" = { bucket_type = "allPrivate", file_name_prefix = "", days_from_hiding_to_deleting = 1, days_from_uploading_to_hiding = null },
   }
 }
 
