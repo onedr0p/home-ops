70a637a4cbcf1d3da95172a561be0b23f3038acd
Devin Buhl <devin@buhl.casa>
2022-04-13 14:41:11 -0400

    fix(cluster): update rook-cep-toolbox
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- cluster/apps/rook-ceph/rook-direct-mount/backup.sh
+++ cluster/apps/rook-ceph/rook-direct-mount/backup.sh
@@ -2,12 +2,14 @@
 
 # PVC=sonarr-config-v1 \
 # NS=media \
-# kubectl -n rook-ceph exec -it (kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/backup-rbd.sh --rbd (k get pv/(kubectl get pv | grep "$PVC" | awk -F' ' '{print $1}') -n "${NS}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$PVC"
+# kubectl -n rook-ceph exec -it (kubectl -n rook-ceph get pod -l "app=rook-direct-mount" -o jsonpath='{.items[0].metadata.name}') -- /scripts/backup.sh --rbd (k get pv/(kubectl get pv | grep "$PVC" | awk -F' ' '{print $1}') -n "${NS}" -o json | jq -rj '.spec.csi.volumeAttributes.imageName') --pvc "$PVC"
 
 # Set defaults
-NFS_MOUNTPATH="/mnt/nas-data"
+NFS_MOUNTPATH="/mnt/backups"
 RBD_MOUNTPATH="/mnt/data"
 CURRENT_DATE=$(date +"%FT%H%M")
+
+# Script parameters
 rbd=""
 pvc=""
 
@@ -39,17 +41,16 @@ if [[ ! -d "${RBD_MOUNTPATH}" ]]; then
     mkdir -p "${RBD_MOUNTPATH}"
 fi
 
-if [[ ! -d "${NFS_MOUNTPATH}/Backups" ]]; then
-    mkdir -p "${NFS_MOUNTPATH}/Backups"
+if [[ ! -d "${NFS_MOUNTPATH}/Manual" ]]; then
+    mkdir -p "${NFS_MOUNTPATH}/Manual"
 fi
 
 if [[ -f "${NFS_MOUNTPATH}/${pvc}-${CURRENT_DATE}.tar.gz" ]]; then
-    echo "File '${NFS_MOUNTPATH}/Backups/${pvc}-${CURRENT_DATE}.tar.gz' already exists"
+    echo "File '${NFS_MOUNTPATH}/Manual/${pvc}-${CURRENT_DATE}.tar.gz' already exists"
     exit 1
 fi
 
-# ceph osd pool ls
 rbd map -p ceph-blockpool "${rbd}" | xargs -I{} mount {} "${RBD_MOUNTPATH}"
-tar czvf "${NFS_MOUNTPATH}/Backups/${pvc}-${CURRENT_DATE}.tar.gz" -C "${RBD_MOUNTPATH}/" .
+tar czvf "${NFS_MOUNTPATH}/Manual/${pvc}-${CURRENT_DATE}.tar.gz" -C "${RBD_MOUNTPATH}/" .
 umount "${RBD_MOUNTPATH}"
 rbd unmap -p ceph-blockpool "${rbd}"
