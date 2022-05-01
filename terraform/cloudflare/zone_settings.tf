9b4825a1f80282fd5f09ca5f1ab6fb89b95eae10
Devin Buhl <devin@buhl.casa>
2022-05-01 10:59:16 -0400

    fix(terraform): set min tls version to 1.2
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- terraform/cloudflare/zone_settings.tf
+++ terraform/cloudflare/zone_settings.tf
@@ -5,7 +5,7 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
     ssl = "strict"
     # /ssl-tls/edge-certificates
     always_use_https         = "on"
-    min_tls_version          = "1.0"
+    min_tls_version          = "1.2"
     opportunistic_encryption = "on"
     tls_1_3                  = "zrt"
     automatic_https_rewrites = "on"
