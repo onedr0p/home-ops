a2092e12a8555b9326da38af49f29c768c160f08
Devin Buhl <devin@buhl.casa>
2022-05-30 21:04:02 -0400

    chore: update valetudo
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- hack/valetudo/vacuum-updater.sh
+++ hack/valetudo/vacuum-updater.sh
@@ -6,19 +6,12 @@
 # have Python installed.
 #
 
-PROJECT_DIR="$(git rev-parse --show-toplevel)"
 VACUUM_USER="root"
 VACUUM_ADDR="192.168.1.28"
 SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR"
 
 # renovate: datasource=github-releases depName=Hypfer/Valetudo
 VALETUDO_VERSION="2022.05.1"
-
-# renovate: datasource=github-releases depName=prometheus/node_exporter
-NODE_EXPORTER_VERSION="v1.3.1"
-
-# renovate: datasource=github-releases depName=vectordotdev/vector
-VECTOR_VERSION="v0.21.2"
 
 #
 # valetudo
@@ -44,60 +37,4 @@ ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
     chmod +x /usr/local/bin/valetudo
     echo "*** Starting Valetudo... ***"
     /etc/init/S11valetudo start > /dev/null 2>&1
-EOL
-
-#
-# node_exporter
-#
-echo "*** Downloading node_exporter... ***"
-curl -fsSL -o "/tmp/node_exporter.tar.gz" \
-    "https://github.com/prometheus/node_exporter/releases/download/${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION#?}.linux-armv7.tar.gz"
-echo "*** Extracting node_exporter ... ***"
-tar zxf /tmp/node_exporter.tar.gz --strip-components=1 -C /tmp
-echo "*** Stopping node_exporter... ***"
-ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
-    /etc/init/S11node_exporter stop > /dev/null 2>&1
-    sleep 10
-EOL
-echo "*** Copying node_exporter to the Vacuum... ***"
-scp -O "${SSH_OPTS}" -q \
-    /tmp/node_exporter \
-    "${VACUUM_USER}@${VACUUM_ADDR}:/mnt/data/node_exporter/node_exporter"
-echo "*** Copying node_exporter init script to the Vacuum... ***"
-scp -O "${SSH_OPTS}" -q \
-    "${PROJECT_DIR}/hack/valetudo/S11node_exporter" \
-    "${VACUUM_USER}@${VACUUM_ADDR}:/etc/init/S11node_exporter"
-ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
-    echo "*** Make node_exporter Executable... ***"
-    chmod +x /mnt/data/node_exporter/node_exporter
-    echo "*** Starting node_exporter... ***"
-    /etc/init/S11node_exporter start > /dev/null 2>&1
-EOL
-
-#
-# vector
-#
-echo "*** Downloading Vector... ***"
-curl -fsSL -o "/tmp/vector.tar.gz" \
-    "https://github.com/vectordotdev/vector/releases/download/${VECTOR_VERSION}/vector-${VECTOR_VERSION#?}-armv7-unknown-linux-musleabihf.tar.gz"
-echo "*** Extracting Vector ... ***"
-tar zxf /tmp/vector.tar.gz --strip-components=1 -C /tmp
-echo "*** Stopping Vector... ***"
-ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
-    /etc/init/S11vector stop > /dev/null 2>&1
-    sleep 10
-EOL
-echo "*** Copying Vector to the Vacuum... ***"
-scp -O "${SSH_OPTS}" -q \
-    /tmp/vector-armv7-unknown-linux-musleabihf/bin/vector \
-    "${VACUUM_USER}@${VACUUM_ADDR}:/mnt/data/vector/vector"
-echo "*** Copying Vector init script to the Vacuum... ***"
-scp -O "${SSH_OPTS}" -q \
-    "${PROJECT_DIR}/hack/valetudo/S11vector" \
-    "${VACUUM_USER}@${VACUUM_ADDR}:/etc/init/S11vector"
-ssh "${SSH_OPTS}" -T "${VACUUM_USER}@${VACUUM_ADDR}" <<'EOL'
-    echo "*** Make Vector Executable... ***"
-    chmod +x /mnt/data/vector/vector
-    echo "*** Starting Vector... ***"
-    /etc/init/S11vector start > /dev/null 2>&1
 EOL
