2291b392704f762e48f44b9cce5691ba610a5ac9
Devin Buhl <devin@buhl.casa>
2022-06-11 16:50:58 -0400

    feat: test out pushovarr script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- cluster/apps/media/scripts/pushover/pushover-notify.sh
+++ cluster/apps/media/scripts/pushover/pushover-notify.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-export PUSHOVARR_PUSHOVER_API_URL="https://api.pushover.net/1/messages.json"
+export PUSHOVER_API_URL="https://api.pushover.net/1/messages.json"
 
 #
 # Application specific variables
 #
-PUSHOVARR_STARR_APP="$(basename --suffix=.pid -- /config/*.pid)"
-PUSHOVARR_STARR_PORT="$(xmlstarlet sel -t -v "//Port" -nl /config/config.xml)"
-PUSHOVARR_STARR_APIKEY="$(xmlstarlet sel -t -v "//ApiKey" -nl /config/config.xml)"
-PUSHOVARR_STARR_EVENT_TYPE="${PUSHOVARR_STARR_APP}_eventtype"
+STARR_APP="$(basename --suffix=.pid -- /config/*.pid)"
+STARR_PORT="$(xmlstarlet sel -t -v "//Port" -nl /config/config.xml)"
+STARR_APIKEY="$(xmlstarlet sel -t -v "//ApiKey" -nl /config/config.xml)"
+STARR_EVENT_TYPE="${STARR_APP}_eventtype"
 # TODO: Not all starr apps support InstanceName yet, this will
-# replace PUSHOVARR_APP_DISPLAY_NAME in the future
-# export PUSHOVARR_STARR_INSTANCE_NAME
-# PUSHOVARR_STARR_INSTANCE_NAME="$(xmlstarlet sel -t -m '/Config/InstanceName' -v Value </config/config.xml)"
+# replace APP_DISPLAY_NAME in the future
+# export STARR_INSTANCE_NAME
+# STARR_INSTANCE_NAME="$(xmlstarlet sel -t -m '/Config/InstanceName' -v Value </config/config.xml)"
 
 #
 # Configurable environment variables
 #
-PUSHOVARR_STARR_INSTANCE_NAME=""      # (required)
-PUSHOVARR_STARR_APP_URL=""            # (required)
-PUSHOVARR_PUSHOVER_USER_KEY=""        # (required)
-PUSHOVARR_PUSHOVER_TOKEN=""           # (required)
-PUSHOVARR_PUSHOVER_DEVICE=""          # (optional)
-PUSHOVARR_PUSHOVER_PRIORITY=""        # (optional)
-PUSHOVARR_PUSHOVER_SOUND=""           # (optional)
+STARR_INSTANCE_NAME=""      # (required)
+STARR_APP_URL=""            # (required)
+PUSHOVER_USER_KEY=""        # (required)
+PUSHOVER_TOKEN=""           # (required)
+PUSHOVER_DEVICE=""          # (optional)
+PUSHOVER_PRIORITY=""        # (optional)
+PUSHOVER_SOUND=""           # (optional)
 
-for variable in ${!PUSHOVARR_*}
+for variable in ${!*}
 do
     declare -n var=$variable
     printf "\"%s=%s\"," "${!var}" "${var}"
@@ -37,7 +37,7 @@ exit 0
 #
 # Send Notification on Test
 #
-if [[ "${!PUSHOVARR_STARR_EVENT_TYPE}" == "Test" ]]; then
+if [[ "${!STARR_EVENT_TYPE}" == "Test" ]]; then
     send "Test Notification" "Test Message"
     exit 0
 fi
@@ -45,7 +45,7 @@ fi
 #
 # Send notification on Download
 #
-if [[ "${!PUSHOVARR_STARR_EVENT_TYPE}" == "Download" ]]; then
+if [[ "${!STARR_EVENT_TYPE}" == "Download" ]]; then
     case "${APPLICATION}" in
         sonarr)
             #shellcheck disable=SC2154
@@ -76,20 +76,20 @@ send() {
     local pushover_message="${2}"
     local msg=
     msg=$(jq -n \
-            --arg token "${PUSHOVARR_PUSHOVER_TOKEN}" \
-            --arg user "${PUSHOVARR_PUSHOVER_USER_KEY}" \
+            --arg token "${PUSHOVER_TOKEN}" \
+            --arg user "${PUSHOVER_USER_KEY}" \
             --arg title "${pushover_title}" \
             --arg message "${pushover_message}" \
-            --arg url "${PUSHOVARR_STARR_APP_URL}" \
-            --arg url_title "View in ${PUSHOVARR_STARR_INSTANCE_NAME}" \
-            --arg priority "${PUSHOVARR_PUSHOVER_PRIORITY}" \
-            --arg sound "${PUSHOVARR_PUSHOVER_SOUND}" \
-            --arg device "${PUSHOVARR_PUSHOVER_DEVICE}" \
+            --arg url "${STARR_APP_URL}" \
+            --arg url_title "View in ${STARR_INSTANCE_NAME}" \
+            --arg priority "${PUSHOVER_PRIORITY}" \
+            --arg sound "${PUSHOVER_SOUND}" \
+            --arg device "${PUSHOVER_DEVICE}" \
             '{token: $token, user: $user, title: $title, message: $message, url: $url, url_title: $url_title, priority: $priority, sound: $sound, device: $device}' \
         )
 
     curl \
         --header "Content-Type: application/json" \
         --data-binary "${msg}" \
-        --request POST "${PUSHOVARR_PUSHOVER_API_URL}"
+        --request POST "${PUSHOVER_API_URL}"
 }
