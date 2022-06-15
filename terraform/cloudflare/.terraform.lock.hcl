278cbde4d762b4c1caace64a856ec9851752b524
Devin Buhl <devin@buhl.casa>
2022-06-15 07:33:52 -0400

    fix(terraform): remove firewall rules due to hass - google assistant not working
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- terraform/cloudflare/.terraform.lock.hcl
+++ terraform/cloudflare/.terraform.lock.hcl
@@ -40,6 +40,20 @@ provider "registry.terraform.io/cloudflare/cloudflare" {
     "h1:eoZmf55yvnC+FKtIlBAoXV9w4+mSyeq87yQkCzjUdt4=",
     "h1:qychzM1hDOIavDcB1VnmQ0Fq4BJ29vmHtawEl5/hwCE=",
     "h1:syBYbVEjef+BZmwU/Nd6IEDjh9Dkl8/QYoWswuaIgnE=",
+    "zh:0db85d83d9c9a0eddfec0a3c59d60c12d94e246f12cb90f57deaadbc080b9cac",
+    "zh:0e3e415afc6a2998dea519d195f71ece17e23f902af4dcf65e936af2dd5662ac",
+    "zh:0f3d2a2d61e50f6481fe3599e428d09af7725ae05a4c8e2b38ab64313a90d45a",
+    "zh:26f972c8d22763f88b2e3bdc7f6a91fb2ceca1fec31c1e56d2624890ce57a28b",
+    "zh:425172555e22c0dc280c713a149ad24919521a0de4003a6550f0550d458be9a8",
+    "zh:55d8468f21f95b77b2e386e1451e1448b44e1d68f4f0b2246274daea9c49b8b4",
+    "zh:55e2d6f662429f4b5e6d4e6f28cc2f6dd4baa8a15b77bd93d6d0bd5d6de0462e",
+    "zh:876b52e3d15b5a48f41b4f237ddbcf1390b46823de08db5c3b6323fe3f446b8a",
+    "zh:8cdcb0c9949a8031eb8ee81534ffce077eb05fcf10524729c527fad28a0e5625",
+    "zh:af74f7912beb86afb27dea35eb1a6d9fbb309f97e0b8157bb7e8d73ed4a8422e",
+    "zh:ba6488af23a6f7a84124243d8522dd0492b5c8197ba31dcdb4c00d50c31e0a3c",
+    "zh:dd92835212fccd82c3fa2613b73a89ca389863d50769f8e923e7e03d2d976630",
+    "zh:dee32bdbe8890f979c615d27deec7c6777c7d2207606d4acf75333a634a07389",
+    "zh:e01707a31768d65005dd78be224a661387adf239082bf85b84dda47d177879ea",
   ]
 }
 
--- terraform/cloudflare/firewall_rules_io.tf
+++ terraform/cloudflare/firewall_rules_io.tf
@@ -2,87 +2,87 @@
 # GeoIP blocking
 #
 
-resource "cloudflare_filter" "countries" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Expression to block all countries except US, CA, NL and AU"
-  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\" and ip.geoip.country ne \"NL\" and ip.geoip.country ne \"AU\")"
-}
+# resource "cloudflare_filter" "countries" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Expression to block all countries except US, CA, NL and AU"
+#   expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\" and ip.geoip.country ne \"NL\" and ip.geoip.country ne \"AU\")"
+# }
 
-resource "cloudflare_firewall_rule" "countries" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Firewall rule to block all countries except US, CA and AU"
-  filter_id   = cloudflare_filter.countries.id
-  action      = "block"
-}
+# resource "cloudflare_firewall_rule" "countries" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Firewall rule to block all countries except US, CA and AU"
+#   filter_id   = cloudflare_filter.countries.id
+#   action      = "block"
+# }
 
-#
-# Bots
-#
+# #
+# # Bots
+# #
 
-resource "cloudflare_filter" "bots" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Expression to block bots determined by CF"
-  expression  = "(cf.client.bot)"
-}
+# resource "cloudflare_filter" "bots" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Expression to block bots determined by CF"
+#   expression  = "(cf.client.bot)"
+# }
 
-resource "cloudflare_firewall_rule" "bots" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Firewall rule to block bots determined by CF"
-  filter_id   = cloudflare_filter.bots.id
-  action      = "block"
-}
+# resource "cloudflare_firewall_rule" "bots" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Firewall rule to block bots determined by CF"
+#   filter_id   = cloudflare_filter.bots.id
+#   action      = "block"
+# }
 
-#
-# Block threats less than Medium
-#
+# #
+# # Block threats less than Medium
+# #
 
-resource "cloudflare_filter" "threats" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Expression to block medium threats"
-  expression  = "(cf.threat_score gt 14)"
-}
+# resource "cloudflare_filter" "threats" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Expression to block medium threats"
+#   expression  = "(cf.threat_score gt 14)"
+# }
 
-resource "cloudflare_firewall_rule" "threats" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Firewall rule to block medium threats"
-  filter_id   = cloudflare_filter.threats.id
-  action      = "block"
-}
+# resource "cloudflare_firewall_rule" "threats" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Firewall rule to block medium threats"
+#   filter_id   = cloudflare_filter.threats.id
+#   action      = "block"
+# }
 
-#
-# UptimeRobot
-#
+# #
+# # UptimeRobot
+# #
 
-data "http" "uptimerobot_ips" {
-  url = "https://uptimerobot.com/inc/files/ips/IPv4.txt"
-}
+# data "http" "uptimerobot_ips" {
+#   url = "https://uptimerobot.com/inc/files/ips/IPv4.txt"
+# }
 
-resource "cloudflare_ip_list" "uptimerobot" {
-  account_id  = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
-  name        = "uptimerobot"
-  kind        = "ip"
-  description = "List of UptimeRobot IP Addresses"
+# resource "cloudflare_ip_list" "uptimerobot" {
+#   account_id  = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
+#   name        = "uptimerobot"
+#   kind        = "ip"
+#   description = "List of UptimeRobot IP Addresses"
 
-  dynamic "item" {
-    for_each = split("\r\n", chomp(data.http.uptimerobot_ips.body))
-    content {
-      value = item.value
-    }
-  }
-}
+#   dynamic "item" {
+#     for_each = split("\r\n", chomp(data.http.uptimerobot_ips.body))
+#     content {
+#       value = item.value
+#     }
+#   }
+# }
 
-resource "cloudflare_filter" "uptimerobot" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Expression to allow UptimeRobot IP addresses"
-  expression  = "(ip.src in $uptimerobot)"
-  depends_on = [
-    cloudflare_ip_list.uptimerobot,
-  ]
-}
+# resource "cloudflare_filter" "uptimerobot" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Expression to allow UptimeRobot IP addresses"
+#   expression  = "(ip.src in $uptimerobot)"
+#   depends_on = [
+#     cloudflare_ip_list.uptimerobot,
+#   ]
+# }
 
-resource "cloudflare_firewall_rule" "uptimerobot" {
-  zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
-  description = "Firewall rule to allow UptimeRobot IP addresses"
-  filter_id   = cloudflare_filter.uptimerobot.id
-  action      = "allow"
-}
+# resource "cloudflare_firewall_rule" "uptimerobot" {
+#   zone_id     = lookup(data.cloudflare_zones.domain_io.zones[0], "id")
+#   description = "Firewall rule to allow UptimeRobot IP addresses"
+#   filter_id   = cloudflare_filter.uptimerobot.id
+#   action      = "allow"
+# }
