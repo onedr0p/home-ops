a5853da2da66484c7e4b6105d708a42daf13d9a8
Devin Buhl <devin@buhl.casa>
2022-07-22 20:46:34 -0400

    feat: add gluctl git submodule
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- scripts/ctl.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zx
-import { $, argv } from 'zx'
-import { commandRunner } from './lib/index.mjs'
-
-argv.debug ? $.verbose = true : $.verbose = false
-
-await commandRunner(argv._[1], argv._[2])
