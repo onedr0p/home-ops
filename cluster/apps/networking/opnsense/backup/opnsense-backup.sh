4cb274fa2d7cb7ef3a542330bd45328691a6b8cd
Devin Buhl <devin@buhl.casa>
2022-08-15 09:12:49 -0400

    fix: update opnsense backup script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- cluster/apps/networking/opnsense/backup/opnsense-backup.sh
+++ cluster/apps/networking/opnsense/backup/opnsense-backup.sh
@@ -4,22 +4,28 @@ set -o nounset
 set -o errexit
 
 config_filename="$(date "+%Y%m%d-%H%M%S").xml"
+
+http_host=${S3_URL#*//}
+http_host=${http_host%:*}
 http_request_date=$(date -R)
 http_filepath="opnsense-backup/${config_filename}"
-sig="PUT\n\ntext/xml\n${http_request_date}\n/${http_filepath}"
-http_signature=$(echo -en "${sig}" | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary | base64)
+http_signature=$(
+    printf "PUT\n\ntext/xml\n%s\n/%s" "${http_request_date}" "${http_filepath}" \
+        | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary \
+        | base64
+)
 
 echo "Download Opnsense config file ..."
 curl -fsSL \
     --user "${OPNSENSE_KEY}:${OPNSENSE_SECRET}" \
     --output "${config_filename}" \
-    "http://192.168.1.1/api/backup/backup/download"
+    "${OPNSENSE_URL}/api/backup/backup/download"
 
 echo "Upload backup to s3 bucket ..."
 curl -fsSL \
     -X PUT -T "${config_filename}" \
-    -H "Host: minio.default.svc.cluster.local" \
+    -H "Host: ${http_host}" \
     -H "Date: ${http_request_date}" \
     -H "Content-Type: text/xml" \
     -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${http_signature}" \
-    "http://minio.default.svc.cluster.local:9000/${http_filepath}"
+    "${S3_URL}/${http_filepath}"
