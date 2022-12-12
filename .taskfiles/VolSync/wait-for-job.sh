179f3cbad27c2c663be089b48362be03e4ce1eb6
Devin Buhl <devin@buhl.casa>
2022-12-12 15:51:25 -0500

    feat: no jq on wait job script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .taskfiles/VolSync/wait-for-job.sh
+++ .taskfiles/VolSync/wait-for-job.sh
@@ -6,7 +6,7 @@ NAMESPACE="${2:-default}"
 [[ -z "${JOB_NAME}" ]] && echo "No Job" && exit 1
 
 while true; do
-    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o json | jq -rc '.items | .[].status.phase')"
+    STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
     if [ "${STATUS}" == "Pending" ]; then
         break
     fi
