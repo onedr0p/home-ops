11486fde28f04b2e49768fd8fbb13e8cd31c20e3
Devin Buhl <devin@buhl.casa>
2022-12-17 15:25:27 -0500

    fix: more depends on for tf
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- terraform/storage/app_nexus_config.tf
+++ terraform/storage/app_nexus_config.tf
@@ -18,6 +18,10 @@ resource "nexus_security_realms" "active_realms" {
 
 resource "nexus_security_anonymous" "system" {
   provider   = nexus.nas
+  depends_on = [
+    kubernetes_stateful_set_v1.nexus,
+    time_sleep.wait
+  ]
   enabled    = true
   user_id    = "anonymous"
   realm_name = "NexusAuthorizingRealm"
