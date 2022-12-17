7b92b0c5174828d676c09d8c0be23e163dfb6645
Devin Buhl <devin@buhl.casa>
2022-12-17 15:24:31 -0500

    feat: Add more mirrors
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- terraform/storage/app_nexus_config.tf
+++ terraform/storage/app_nexus_config.tf
@@ -61,6 +61,34 @@ module "docker_proxy_docker_mirror" {
   registry_remote_url = "https://registry-1.docker.io"
 }
 
+module "docker_proxy_ecr_mirror" {
+  depends_on = [
+    kubernetes_stateful_set_v1.nexus,
+    time_sleep.wait
+  ]
+  source = "./modules/docker_proxy"
+  providers = {
+    nexus = nexus.nas
+  }
+  registry_name       = "ecr-mirror"
+  registry_index_type = "REGISTRY"
+  registry_remote_url = "https://public.ecr.aws"
+}
+
+module "docker_proxy_gcr_mirror" {
+  depends_on = [
+    kubernetes_stateful_set_v1.nexus,
+    time_sleep.wait
+  ]
+  source = "./modules/docker_proxy"
+  providers = {
+    nexus = nexus.nas
+  }
+  registry_name       = "gcr-mirror"
+  registry_index_type = "REGISTRY"
+  registry_remote_url = "https://gcr.io"
+}
+
 module "docker_proxy_ghcr_mirror" {
   depends_on = [
     kubernetes_stateful_set_v1.nexus,
@@ -89,6 +117,20 @@ module "docker_proxy_k8s_mirror" {
   registry_remote_url = "https://registry.k8s.io"
 }
 
+module "docker_proxy_k8s_gcr_mirror" {
+  depends_on = [
+    kubernetes_stateful_set_v1.nexus,
+    time_sleep.wait
+  ]
+  source = "./modules/docker_proxy"
+  providers = {
+    nexus = nexus.nas
+  }
+  registry_name       = "k8s-gcr-mirror"
+  registry_index_type = "REGISTRY"
+  registry_remote_url = "https://k8s.gcr.io"
+}
+
 module "docker_proxy_quay_mirror" {
   depends_on = [
     kubernetes_stateful_set_v1.nexus,
@@ -107,8 +149,11 @@ resource "nexus_repository_docker_group" "docker_group" {
   provider = nexus.nas
   depends_on = [
     module.docker_proxy_docker_mirror,
+    module.docker_proxy_ecr_mirror,
+    module.docker_proxy_gcr_mirror,
     module.docker_proxy_ghcr_mirror,
     module.docker_proxy_k8s_mirror,
+    module.docker_proxy_k8s_gcr_mirror,
     module.docker_proxy_quay_mirror
   ]
   name   = "docker-group"
@@ -122,8 +167,11 @@ resource "nexus_repository_docker_group" "docker_group" {
   group {
     member_names = [
       module.docker_proxy_docker_mirror.registry_name,
+      module.docker_proxy_ecr_mirror.registry_name,
+      module.docker_proxy_gcr_mirror.registry_name,
       module.docker_proxy_ghcr_mirror.registry_name,
       module.docker_proxy_k8s_mirror.registry_name,
+      module.docker_proxy_k8s_gcr_mirror.registry_name,
       module.docker_proxy_quay_mirror.registry_name
     ]
   }
