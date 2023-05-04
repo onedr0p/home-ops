600c64029a1236b24a120303a51158b9962440b7
Devin Buhl <devin@buhl.casa>
2023-05-04 16:17:18 -0400

    feat: remove cloudflare proxied networks workflow and script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/cloudflareNetworks.mjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zx
-$.verbose = false
-const response = await fetch('https://api.cloudflare.com/client/v4/ips')
-const body = await response.json()
-const ips = body.result.ipv4_cidrs.concat(body.result.ipv6_cidrs);
-echo(ips.join("\\,"))
