5ab6713d2f9b901e56c03e5fbdce3b16c124d113
Devin Buhl <devin@buhl.casa>
2023-05-04 21:12:36 -0400

    feat: add terraform for cloduflare firewall
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- provision/kubernetes/cloudflare/domain_ingress.tf
+++ provision/kubernetes/cloudflare/domain_ingress.tf
@@ -80,3 +80,91 @@ resource "cloudflare_zone_settings_override" "public_domain_settings" {
     }
   }
 }
+
+resource "cloudflare_filter" "public_domain_github_flux_webhook" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Allow GitHub flux API"
+  expression  = "(ip.geoip.asnum eq 36459 and http.host eq \"flux-webhook.devbu.io\")"
+}
+
+resource "cloudflare_firewall_rule" "public_domain_github_flux_webhook" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Allow GitHub flux API"
+  filter_id   = cloudflare_filter.public_domain_github_flux_webhook.id
+  action      = "allow"
+  priority    = 1
+}
+
+#
+# GeoIP blocking
+#
+
+resource "cloudflare_filter" "countries" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Expression to block all countries except US, CA, PH and AU"
+  expression  = "(ip.geoip.country ne \"US\" and ip.geoip.country ne \"CA\" and ip.geoip.country ne \"AU\" and ip.geoip.country ne \"PH\")"
+}
+
+resource "cloudflare_firewall_rule" "countries" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Firewall rule to block all countries except US, CA, PH, and AU"
+  filter_id   = cloudflare_filter.countries.id
+  action      = "block"
+}
+
+#
+# Bots
+#
+
+resource "cloudflare_filter" "bots" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Expression to block bots determined by CF"
+  expression  = "(cf.client.bot) or (cf.threat_score gt 14)"
+}
+
+resource "cloudflare_firewall_rule" "bots" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Firewall rule to block bots determined by CF"
+  filter_id   = cloudflare_filter.bots.id
+  action      = "block"
+}
+
+#
+# UptimeRobot
+#
+
+data "http" "uptimerobot_ips" {
+  url = "https://uptimerobot.com/inc/files/ips/IPv4.txt"
+}
+
+resource "cloudflare_list" "uptimerobot" {
+  account_id  = data.sops_file.secrets.data["cloudflare_account_id"]
+  name        = "uptimerobot"
+  kind        = "ip"
+  description = "List of UptimeRobot IP Addresses"
+
+  dynamic "item" {
+    for_each = split("\r\n", chomp(data.http.uptimerobot_ips.response_body))
+    content {
+      value {
+        ip = item.value
+      }
+    }
+  }
+}
+
+resource "cloudflare_filter" "uptimerobot" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Expression to allow UptimeRobot IP addresses"
+  expression  = "(ip.src in $uptimerobot)"
+  depends_on = [
+    cloudflare_list.uptimerobot,
+  ]
+}
+
+resource "cloudflare_firewall_rule" "uptimerobot" {
+  zone_id     = data.cloudflare_zone.public_domain.id
+  description = "Firewall rule to allow UptimeRobot IP addresses"
+  filter_id   = cloudflare_filter.uptimerobot.id
+  action      = "allow"
+}
