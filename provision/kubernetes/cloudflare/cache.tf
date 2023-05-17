90b8591fdb25416b0959ab34efa428cccaf3698e
Devin Buhl <devin@buhl.casa>
2023-05-17 09:05:34 -0400

    chore: cache rules are not part of TF CF provider yet
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- provision/kubernetes/cloudflare/cache.tf
+++ provision/kubernetes/cloudflare/cache.tf
@@ -1,11 +1,1 @@
-resource "cloudflare_page_rule" "bypass_cache_plex" {
-  zone_id  = data.cloudflare_zone.domain.id
-  target   = format("plex.%s/*", data.cloudflare_zone.domain.name)
-  status   = "active"
-  priority = 1
-
-  actions {
-    cache_level         = "bypass"
-    disable_performance = true
-  }
-}
+# TODO: Migrate over Cache Rules to Terraform once implemented in the provider
