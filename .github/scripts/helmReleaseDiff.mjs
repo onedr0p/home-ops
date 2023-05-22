5ca73a5e518a8610fb408a70db5e2839bbdfe125
Devin Buhl <devin@buhl.casa>
2023-05-22 11:25:36 -0400

    feat: support OCI in helmReleaseDiff.mjs
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/helmReleaseDiff.mjs
+++ .github/scripts/helmReleaseDiff.mjs
@@ -8,7 +8,9 @@ $.verbose = false
  * @param --incoming-release  The target Flux HelmRelease to compare against the source
  * @param --kubernetes-dir    The directory containing your Flux manifests including the HelmRepository manifests
  * * Limitations:
- * * Does not work with multiple HelmRelease maninfests in the same YAML document
+ * * Does not work with multiple HelmRelease manifests in the same YAML document
+ * * All Kubernetes manifests must be in a single top level directory (nested folders under that is fine)
+ * * Requires kustomization.yaml files in-order to build the full helm release
  */
 const CurrentRelease  = argv['current-release']
 const IncomingRelease = argv['incoming-release']
@@ -18,7 +20,7 @@ const dyff      = await which('dyff')
 const helm      = await which('helm')
 const kustomize = await which('kustomize')
 
-async function helmRelease(releaseFile) {
+async function helmRelease (releaseFile) {
   const helmRelease = await fs.readFile(releaseFile, 'utf8')
   const doc = YAML.parseAllDocuments(helmRelease).map((item) => item.toJS())
   const release = doc.filter((item) =>
@@ -28,21 +30,21 @@ async function helmRelease(releaseFile) {
   return release[0]
 }
 
-async function helmRepositoryUrl(kubernetesDir, releaseName) {
+async function helmRepository (kubernetesDir, releaseName) {
   const files = await globby([`${kubernetesDir}/**/*.yaml`])
   for await (const file of files) {
     const contents = await fs.readFile(file, 'utf8')
-    const doc = YAML.parseAllDocuments(contents).map((item) => item.toJS())
-    if ('apiVersion' in doc[0] && doc[0].apiVersion === 'source.toolkit.fluxcd.io/v1beta2'
-        && 'kind' in doc[0] && doc[0].kind === 'HelmRepository'
-        && 'metadata' in doc[0] && 'name' in doc[0].metadata && doc[0].metadata.name === releaseName)
+    const repository = YAML.parseAllDocuments(contents).map((item) => item.toJS())
+    if (repository[0] && 'apiVersion' in repository[0] && repository[0].apiVersion === 'source.toolkit.fluxcd.io/v1beta2'
+        && 'kind' in repository[0] && repository[0].kind === 'HelmRepository'
+        && 'metadata' in repository[0] && 'name' in repository[0].metadata && repository[0].metadata.name === releaseName)
     {
-      return doc[0].spec.url
+      return repository[0]
     }
   }
 }
 
-async function kustomizeBuild(releaseBaseDir, releaseName) {
+async function kustomizeBuild (releaseBaseDir, releaseName) {
   const build = await $`${kustomize} build --load-restrictor=LoadRestrictionsNone ${releaseBaseDir}`
   const docs = YAML.parseAllDocuments(build.stdout).map((item) => item.toJS())
   const release = docs.filter((item) =>
@@ -53,18 +55,23 @@ async function kustomizeBuild(releaseBaseDir, releaseName) {
   return release[0]
 }
 
-async function helmRepoAdd (registryName, registryUrl) {
-  await $`${helm} repo add ${registryName} ${registryUrl}`
-}
-
-async function helmTemplate (releaseName, registryName, chartName, chartVersion, chartValues) {
+async function helmTemplate (release, repository) {
   const values = new YAML.Document()
-  values.contents = chartValues
+  values.contents = release.spec.values
   const valuesFile = await $`mktemp`
   await fs.writeFile(valuesFile.stdout.trim(), values.toString())
 
-  const manifestsFile = await $`mktemp`
-  const manifests = await $`${helm} template --kube-version 1.24.8 --release-name ${releaseName} --include-crds=false ${registryName}/${chartName} --version ${chartVersion} --values ${valuesFile.stdout.trim()}`
+  // Template out our helm values into Kubernetes manifests
+  let manifests
+  if ('type' in repository.spec && repository.spec.type == 'oci') {
+    console.log(`${helm} template --kube-version 1.27.1 --release-name ${release.metadata.name} --include-crds=false ${repository.spec.url}/${release.spec.chart.spec.chart} --version ${release.spec.chart.spec.version} --values ${valuesFile.stdout.trim()}`)
+    manifests = await $`${helm} template --kube-version 1.27.1 --release-name ${release.metadata.name} --include-crds=false ${repository.spec.url}/${release.spec.chart.spec.chart} --version ${release.spec.chart.spec.version} --values ${valuesFile.stdout.trim()}`
+  } else {
+    console.log(`${helm} repo add ${release.spec.chart.spec.sourceRef.name} ${repository.spec.url}`)
+    await $`${helm} repo add ${release.spec.chart.spec.sourceRef.name} ${repository.spec.url}`
+    console.log(`${helm} template --kube-version 1.27.1 --release-name ${release.metadata.name} --include-crds=false ${release.spec.chart.spec.sourceRef.name}/${release.spec.chart.spec.chart} --version ${release.spec.chart.spec.version} --values ${valuesFile.stdout.trim()}`)
+    manifests = await $`${helm} template --kube-version 1.27.1 --release-name ${release.metadata.name} --include-crds=false ${release.spec.chart.spec.sourceRef.name}/${release.spec.chart.spec.chart} --version ${release.spec.chart.spec.version} --values ${valuesFile.stdout.trim()}`
+  }
 
   // Remove docs that are CustomResourceDefinition and keys which contain generated fields
   let documents = YAML.parseAllDocuments(manifests.stdout.trim())
@@ -76,6 +83,7 @@ async function helmTemplate (releaseName, registryName, chartName, chartVersion,
     del(['spec', 'template', 'metadata', 'labels'])
   })
 
+  const manifestsFile = await $`mktemp`
   await fs.writeFile(manifestsFile.stdout.trim(), documents.map(doc => doc.toString({directives: true})).join('\n'))
   return manifestsFile.stdout.trim()
 }
@@ -83,28 +91,14 @@ async function helmTemplate (releaseName, registryName, chartName, chartVersion,
 // Generate current template from Helm values
 const currentRelease = await helmRelease(CurrentRelease)
 const currentBuild = await kustomizeBuild(path.dirname(CurrentRelease), currentRelease.metadata.name)
-const currentRepositoryUrl = await helmRepositoryUrl(KubernetesDir, currentBuild.spec.chart.spec.sourceRef.name)
-await helmRepoAdd(currentBuild.spec.chart.spec.sourceRef.name, currentRepositoryUrl)
-const currentManifests = await helmTemplate(
-  currentBuild.metadata.name,
-  currentBuild.spec.chart.spec.sourceRef.name,
-  currentBuild.spec.chart.spec.chart,
-  currentBuild.spec.chart.spec.version,
-  currentBuild.spec.values
-)
+const currentRepository = await helmRepository(KubernetesDir, currentBuild.spec.chart.spec.sourceRef.name)
+const currentManifests = await helmTemplate(currentBuild, currentRepository)
 
 // Generate incoming template from Helm values
 const incomingRelease = await helmRelease(IncomingRelease)
 const incomingBuild = await kustomizeBuild(path.dirname(IncomingRelease), incomingRelease.metadata.name)
-const incomingRepositoryUrl = await helmRepositoryUrl(KubernetesDir, incomingBuild.spec.chart.spec.sourceRef.name)
-await helmRepoAdd(incomingBuild.spec.chart.spec.sourceRef.name, incomingRepositoryUrl)
-const incomingManifests = await helmTemplate(
-  incomingBuild.metadata.name,
-  incomingBuild.spec.chart.spec.sourceRef.name,
-  incomingBuild.spec.chart.spec.chart,
-  incomingBuild.spec.chart.spec.version,
-  incomingBuild.spec.values
-)
+const incomingRepository = await helmRepository(KubernetesDir, incomingBuild.spec.chart.spec.sourceRef.name)
+const incomingManifests = await helmTemplate(incomingBuild, incomingRepository)
 
 // Print diff using dyff
 const diff = await $`${dyff} --color=off --truecolor=off between --omit-header --ignore-order-changes --detect-kubernetes=true --output=human ${currentManifests} ${incomingManifests}`
