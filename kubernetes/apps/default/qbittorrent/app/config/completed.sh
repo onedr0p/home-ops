5372a73b05e22ff76ab4e1388a0c8bbed33740ba
Devin Buhl <devin@buhl.casa>
2023-06-13 21:21:09 -0400

    fix: qb completed script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-TORRENT_PATH="${1}"
-TORRENT_NAME="${2}"
-/bin/chmod -R 750 "${TORRENT_PATH}"
-/usr/bin/curl -X POST --data-urlencode "name=${TORRENT_NAME}" http://localhost:2468/api/webhook
+/bin/chmod -R 750 "$1"
+/usr/bin/curl -X POST --data-urlencode "name=$2" http://localhost:2468/api/webhook
