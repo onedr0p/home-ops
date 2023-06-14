93dc5fb0f82bf091d36d23913aa8d1c7c4e643d2
Devin Buhl <devin@buhl.casa>
2023-06-13 20:35:35 -0400

    feat: cross-seed go brr
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/config.js
+++ kubernetes/apps/default/qbittorrent/app/config/config.js
@@ -18,7 +18,7 @@ module.exports = {
   includeEpisodes: true,
   includeNonVideos: true,
   duplicateCategories: true,
-  outputDir: "/config/cross-seed",
+  outputDir: "/config/cross-seeds",
   torrentDir: "/config/qBittorrent/BT_backup",
   qbittorrentUrl: "http://localhost",
   rssCadence: "15 minutes", // autobrr doesnt get every announcement
