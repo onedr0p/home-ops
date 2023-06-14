b54454bb41ec2dd28b2a26c1f06d9cdd70a2a5bf
Devin Buhl <devin@buhl.casa>
2023-06-13 21:59:02 -0400

    fix: use infoHash on cross-seed
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/chmod -R 750 "$1"
-/usr/bin/curl -X POST --data-urlencode "name=$2" http://localhost:2468/api/webhook
+/usr/bin/curl -X POST --data-urlencode "infoHash=$2" http://localhost:2468/api/webhook
