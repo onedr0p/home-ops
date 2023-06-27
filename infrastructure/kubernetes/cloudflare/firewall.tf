59943b21046fc24a67907a1c71bd4306eee0c3bc
Devin Buhl <devin@buhl.casa>
2023-06-27 08:27:34 -0400

    fix: block north korea
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- infrastructure/kubernetes/cloudflare/firewall.tf
+++ infrastructure/kubernetes/cloudflare/firewall.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_filter" "block_countries" {
   zone_id     = data.cloudflare_zone.domain.id
   description = "Expression to block countries"
-  expression  = "(ip.geoip.country in {\"CN\" \"IN\" \"RU\"})"
+  expression  = "(ip.geoip.country in {\"CN\" \"IN\" \"KP\" \"RU\"})"
 }
 resource "cloudflare_firewall_rule" "block_countries" {
   zone_id     = data.cloudflare_zone.domain.id
