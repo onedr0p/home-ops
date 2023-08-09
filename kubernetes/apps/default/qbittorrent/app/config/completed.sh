32285407fed8a245c2e57511bfa04e87cf1b0de8
Devin Buhl <devin@buhl.casa>
2023-08-09 18:31:01 -0400

    feat: archive autobrr and cross-seed config
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -2,4 +2,3 @@
 # qBittorrent settings > 'Run external program on torrent finished'
 # /scripts/completed.sh "%F" "%I"
 /bin/chmod -R 750 "$1"
-# /usr/bin/curl -X POST --data-urlencode "infoHash=$2" http://localhost:2468/api/webhook
