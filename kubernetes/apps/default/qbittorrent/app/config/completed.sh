a851ee9df613f15301267c51ee54e6abfb104186
Devin Buhl <devin@buhl.casa>
2023-08-15 13:39:32 -0400

    feat: set up autobrr again
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # qBittorrent settings > 'Run external program on torrent finished'
-# /scripts/completed.sh "%F" "%I"
+# /scripts/completed.sh "%F"
 /bin/chmod -R 750 "$1"
-/usr/bin/curl -X POST --data-urlencode "infoHash=$2" http://localhost:2468/api/webhook
+/usr/bin/curl -X POST --data-urlencode "path=$1" http://localhost:2468/api/webhook
+# qBittorrent settings > 'Run external program on torrent finished'
+# /scripts/completed.sh "%F" "%I"
+# /bin/chmod -R 750 "$1"
+# /usr/bin/curl -X POST --data-urlencode "infoHash=$2" http://localhost:2468/api/webhook
