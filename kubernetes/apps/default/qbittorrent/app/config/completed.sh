f954823e27140306dde3f29a95459877955f88f1
Devin Buhl <devin@buhl.casa>
2023-08-15 11:46:52 -0400

    feat: set up cross-seed with qb
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -2,3 +2,4 @@
 # qBittorrent settings > 'Run external program on torrent finished'
 # /scripts/completed.sh "%F" "%I"
 /bin/chmod -R 750 "$1"
+/usr/bin/curl -X POST --data-urlencode "infoHash=$2" http://localhost:2468/api/webhook
