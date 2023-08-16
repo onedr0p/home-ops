b1bdef3fb7fe225dd8abd6cc6581282dda15517b
Devin Buhl <devin@buhl.casa>
2023-08-16 06:53:33 -0400

    fix: update cross seed config
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/config.js
+++ kubernetes/apps/default/qbittorrent/app/config/config.js
@@ -3,14 +3,15 @@ module.exports = {
   qbittorrentUrl: "http://localhost",
   torznab: [
     "http://prowlarr.default.svc.cluster.local/1/api?apikey={{ .PROWLARR__API_KEY }}",  // fl
-    "http://prowlarr.default.svc.cluster.local/11/api?apikey={{ .PROWLARR__API_KEY }}", // btn
     "http://prowlarr.default.svc.cluster.local/2/api?apikey={{ .PROWLARR__API_KEY }}",  // avz
+    "http://prowlarr.default.svc.cluster.local/6/api?apikey={{ .PROWLARR__API_KEY }}",  // st
+    "http://prowlarr.default.svc.cluster.local/8/api?apikey={{ .PROWLARR__API_KEY }}",  // ptp
+    "http://prowlarr.default.svc.cluster.local/11/api?apikey={{ .PROWLARR__API_KEY }}", // btn
     "http://prowlarr.default.svc.cluster.local/20/api?apikey={{ .PROWLARR__API_KEY }}", // tl
     "http://prowlarr.default.svc.cluster.local/21/api?apikey={{ .PROWLARR__API_KEY }}", // blu
     "http://prowlarr.default.svc.cluster.local/26/api?apikey={{ .PROWLARR__API_KEY }}", // mtv
     "http://prowlarr.default.svc.cluster.local/38/api?apikey={{ .PROWLARR__API_KEY }}", // ipt
-    "http://prowlarr.default.svc.cluster.local/6/api?apikey={{ .PROWLARR__API_KEY }}",  // st
-    // "http://prowlarr.default.svc.cluster.local/8/api?apikey={{ .PROWLARR__API_KEY }}",  // ptp
+    "http://prowlarr.default.svc.cluster.local/39/api?apikey={{ .PROWLARR__API_KEY }}", // hds
   ],
   action: "inject",
   includeEpisodes: true,
@@ -18,7 +19,7 @@ module.exports = {
   duplicateCategories: true,
   matchMode: "safe",
   skipRecheck: true,
-  linkType: "symlink",
+  linkType: "hardlink",
   linkDir: "/media/Downloads/qbittorrent/complete/cross-seed",
   dataDirs: [
     "/media/Downloads/qbittorrent/complete/prowlarr",
