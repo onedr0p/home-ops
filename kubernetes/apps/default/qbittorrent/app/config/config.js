a763d4f1fa668b647f829e044940faaf0ea8e219
Devin Buhl <devin@buhl.casa>
2023-08-19 10:07:17 -0400

    fix: update cross seed config
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/config.js
+++ kubernetes/apps/default/qbittorrent/app/config/config.js
@@ -15,6 +15,7 @@ module.exports = {
     "http://prowlarr.default.svc.cluster.local/40/api?apikey={{ .PROWLARR__API_KEY }}", // uhdb
     "http://prowlarr.default.svc.cluster.local/41/api?apikey={{ .PROWLARR__API_KEY }}", // ts
     "http://prowlarr.default.svc.cluster.local/42/api?apikey={{ .PROWLARR__API_KEY }}", // phd
+    "http://prowlarr.default.svc.cluster.local/44/api?apikey={{ .PROWLARR__API_KEY }}", // hdt
   ],
   action: "inject",
   includeEpisodes: true,
