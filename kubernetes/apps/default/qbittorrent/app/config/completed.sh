426cfc6658abeaa10441bd20b46abe1fe5087f11
Devin Buhl <devin@buhl.casa>
2023-08-26 11:19:51 -0400

    feat: Migrate qb to deployment/cephfs due to cross-seed pvc sharing
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -2,8 +2,4 @@
 # qBittorrent settings > 'Run external program on torrent finished'
 # /scripts/completed.sh "%F"
 /bin/chmod -R 750 "$1"
-/usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://localhost:2468/api/webhook
-# qBittorrent settings > 'Run external program on torrent finished'
-# /scripts/completed.sh "%F" "%I"
-# /bin/chmod -R 750 "$1"
-# /usr/bin/curl -X POST --data-urlencode "infoHash=$2" http://localhost:2468/api/webhook
+/usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://cross-seed.default.svc.cluster.local:2468/api/webhook
