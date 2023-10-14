c128f507877e833e98144152d948f9f9a7f7f709
Devin Buhl <devin@buhl.casa>
2023-10-14 17:00:20 -0400

    fix: do not cross-seed torrents with skip-cross-seed tag
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # qBittorrent settings > 'Run external program on torrent finished'
-# /scripts/completed.sh "%F"
+# /scripts/completed.sh "%F" "%G"
 /bin/chmod -R 750 "$1"
-/usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://cross-seed.default.svc.cluster.local:2468/api/webhook
+if [[ "$2" != *"skip-cross-seed"* ]]; then
+    /usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://cross-seed.default.svc.cluster.local:2468/api/webhook
+fi
+exit 0
