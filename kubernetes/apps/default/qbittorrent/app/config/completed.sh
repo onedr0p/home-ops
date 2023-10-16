2def7f836c35c6c43becbf4ec530ba2383b2b140
Devin Buhl <devin@buhl.casa>
2023-10-16 12:26:41 -0400

    fix: update qb completed script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -2,7 +2,19 @@
 # qBittorrent settings > 'Run external program on torrent finished'
 # /scripts/completed.sh "%F" "%G"
 /bin/chmod -R 750 "$1"
-if [[ "$2" != *"skip-cross-seed"* ]]; then
-    /usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://cross-seed.default.svc.cluster.local:2468/api/webhook
+if [[ "$2" == *"skip-cross-seed"* ]]; then
+    printf "Skipping cross-seed check for '%s' with tags '%s'\n" "$1" "$2"
+else
+    printf "Searching cross-seed for '%s' with tags '%s'\n" "$1" "$2"
+    /usr/bin/curl \
+        --silent \
+        --connect-timeout 5 \
+        --max-time 10 \
+        --retry 5 \
+        --retry-delay 0 \
+        --retry-max-time 40 \
+        --request POST \
+        --data-urlencode "path=$1" \
+        http://cross-seed.default.svc.cluster.local:2468/api/webhook
 fi
 exit 0
