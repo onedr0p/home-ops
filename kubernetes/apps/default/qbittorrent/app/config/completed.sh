bcb89ab33f1b567d0edab2bc21e53d9b34176b3b
Devin Buhl <devin@buhl.casa>
2023-11-02 11:45:19 -0400

    fix: only search cross seed on tag force-cross-seed
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/config/completed.sh
+++ kubernetes/apps/default/qbittorrent/app/config/completed.sh
@@ -2,9 +2,7 @@
 # qBittorrent settings > 'Run external program on torrent finished'
 # /scripts/completed.sh "%F" "%G"
 /bin/chmod -R 750 "$1"
-if [[ "$2" == *"skip-cross-seed"* ]]; then
-    printf "Skipping cross-seed check for '%s' with tags '%s'\n" "$1" "$2"
-else
+if [[ "$2" == *"force-cross-seed"* ]]; then
     printf "Searching cross-seed for '%s' with tags '%s'\n" "$1" "$2"
     /usr/bin/curl \
         --silent \
@@ -15,6 +13,8 @@ else
         --retry-max-time 40 \
         --request POST \
         --data-urlencode "path=$1" \
-        http://cross-seed.default.svc.cluster.local:2468/api/webhook
+        http://cross-seed.default.svc.cluster.local/api/webhook
+else
+    printf "Skipping cross-seed check for '%s' with tags '%s'\n" "$1" "$2"
 fi
 exit 0
