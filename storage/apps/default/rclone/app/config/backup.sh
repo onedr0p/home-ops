290f4424781a2f7deb4e8c85ad376ae5c84a66c0
Devin Buhl <devin@buhl.casa>
2023-11-18 14:02:37 -0500

    feat: add rclone volsync sync to storage cluster
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- storage/apps/default/rclone/app/config/backup.sh
+++ storage/apps/default/rclone/app/config/backup.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 # Sync from MinIO to Cloudflare
-# rclone sync --verbose --create-empty-src-dirs --transfers 10 \
-#     minio:/volsync \
-#     cloudflare:/volsync
+rclone sync --verbose --create-empty-src-dirs --transfers 10 \
+    minio:/volsync \
+    cloudflare:/volsync
 
 # Verify sync was successful
 for app in $(rclone lsf --dirs-only cloudflare:/volsync); do
