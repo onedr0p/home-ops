5b788a8a044f69f8b6f8a76bc039c857c359d2d7
Devin Buhl <devin@buhl.casa>
2023-11-18 14:02:28 -0500

    feat: add rclone volsync sync to storage cluster
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- storage/apps/default/rclone/app/config/backup.sh
+++ storage/apps/default/rclone/app/config/backup.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-export RESTIC_PASSWORD="electrometallurgical-thaumaturgist-lucriferousness"
-export RESTIC_REPOSITORY_TEMPLATE="s3:https://0a10060d9ff3ad5770343527ec45f104.r2.cloudflarestorage.com/volsync"
-export AWS_ACCESS_KEY_ID="0174ad8908c6a60ab24663d200a8b208"
-export AWS_SECRET_ACCESS_KEY="99d38c81b657eb5642cb2adf60042befac214fb36a61fbd0eff82c3f93d72b11"
-
 # Sync from MinIO to Cloudflare
 # rclone sync --verbose --create-empty-src-dirs --transfers 10 \
 #     minio:/volsync \
