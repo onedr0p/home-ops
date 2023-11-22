a5aaabc6009ba456a7701fecb74799de23fb8a73
Devin Buhl <devin@buhl.casa>
2023-11-22 10:26:34 -0500

    feat: add user/groups and add conroller bash script to volsync taskfile
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ .taskfiles/VolSync/scripts/controller.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+APP=$1
+NAMESPACE="${2:-default}"
+
+is_deployment() {
+    kubectl -n "${NAMESPACE}" get deployment "${APP}" >/dev/null 2>&1
+}
+
+is_statefulset() {
+    kubectl -n "${NAMESPACE}" get statefulset "${APP}" >/dev/null 2>&1
+}
+
+if is_deployment; then
+    echo "deployment.apps/${APP}"
+elif is_statefulset; then
+    echo "statefulset.apps/${APP}"
+else
+    echo "No deployment or statefulset found for ${APP}"
+    exit 1
+fi
--- .taskfiles/VolSync/scripts/wait.sh
+++ .taskfiles/VolSync/scripts/wait.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 JOB=$1
 NAMESPACE="${2:-default}"
+
 [[ -z "${JOB}" ]] && echo "Job name not specified" && exit 1
 while true; do
     STATUS="$(kubectl -n "${NAMESPACE}" get pod -l job-name="${JOB}" -o jsonpath='{.items[*].status.phase}')"
