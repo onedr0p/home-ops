13ffbd85f0389dda60c409cf82ae26fb462e1f4b
Devin Buhl <devin@buhl.casa>
2023-12-20 21:14:27 -0500

    chore: update github workflows
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .github/scripts/extract-crds.sh
+++ .github/scripts/extract-crds.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env bash
-# Source: https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/Utilities/crd-extractor.sh
-set -o errexit
 
-kubectl get nodes
+# Check if python3 is installed
+if ! command -v python3 &> /dev/null; then
+    printf "python3 is required for this utility, and is not installed on your machine"
+    printf "please visit https://www.python.org/downloads/ to install it"
+    exit 1
+fi
+# Check if kubectl is installed
+if ! command -v kubectl &> /dev/null; then
+    printf "kubectl is required for this utility, and is not installed on your machine"
+    printf "please visit https://kubernetes.io/docs/tasks/tools/#kubectl to install it"
+    exit 1
+fi
+
+# Check if the pyyaml module is installed
+if ! echo 'import yaml' | python3 &> /dev/null; then
+    printf "the python3 module 'yaml' is required, and is not installed on your machine.\n"
+
+    while true; do
+        read -p "Do you wish to install this program? (y/n) " yn
+        case $yn in
+            [Yy] ) pip3 install pyyaml; break;;
+            "" ) pip3 install pyyaml; break;;
+            [Nn] ) echo "Exiting..."; exit;;
+            * ) echo "Please answer 'y' (yes) or 'n' (no).";;
+        esac
+    done
+fi
 
 # Create temp folder for CRDs
 TMP_CRD_DIR=$HOME/.datree/crds
@@ -34,13 +58,13 @@ do
     # Save name and group for later directory organization
     CRD_GROUPS["$resourceKind"]="$resourceGroup"
 
-    let ++NUM_OF_CRDS
+    ((++NUM_OF_CRDS)) || true
 done < <(kubectl get crds 2>&1 | sed -n '/NAME/,$p' | tail -n +2)
 
 # If no CRDs exist in the cluster, exit
 if [ $NUM_OF_CRDS == 0 ]; then
     printf "No CRDs found in the cluster, exiting...\n"
-    exit 1
+    exit 0
 fi
 
 # Download converter script
