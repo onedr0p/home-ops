e397f746a6e5024c5d7b07cdbc163c5db6c7fe0d
Devin Buhl <devin@buhl.casa>
2024-01-22 08:38:11 -0500

    feat: add k0s test cluster
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ kubernetes/test/k0s/resources/reset-system.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+set -o noglob
+
+[ $(id -u) -eq 0 ] || exec sudo $0 $@
+
+# Remove containerd config
+rm -rf /etc/k0s/containerd.d
+
+# Remove local storage data
+rm -rf /var/openebs/local
+
+# Reboot
+(sleep 10 && systemctl reboot)&
