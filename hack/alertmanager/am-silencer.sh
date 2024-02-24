16ab68515679021adabd9d5ad0ab2b1cd1f11dbb
Devin Buhl <devin@buhl.casa>
2024-02-24 09:52:03 -0500

    fix: add am silencer hack
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- hack/alertmanager/am-silencer.sh
+++ hack/alertmanager/am-silencer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
 # Expire all silences in Alertmanager
 silence_ids=$(curl -s "https://alertmanager.devbu.io/api/v2/silences?silenced=false&inhibited=false&active=true" | jq -r '.[] | .id')
 for id in $silence_ids; do
@@ -8,7 +10,7 @@ for id in $silence_ids; do
 done
 
 # Apply silences to Alertmanager
-find ./hack/alertmanager -type f -print0 | \
+find "${SCRIPT_DIR}" -type f -name "*.json" -print0 | \
     xargs -0 -I{} \
         curl -H "Content-Type: application/json" -d @{} \
             https://alertmanager.devbu.io/api/v2/silences
