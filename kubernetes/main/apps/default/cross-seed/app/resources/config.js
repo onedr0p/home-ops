ac08e9f53769aafea0c0fcbfa14746c263e76415
Devin Buhl <devin@buhl.casa>
2024-03-07 07:32:28 -0500

    fix: update cross-seed config
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/main/apps/default/cross-seed/app/resources/config.js
+++ kubernetes/main/apps/default/cross-seed/app/resources/config.js
@@ -6,10 +6,14 @@ module.exports = {
     `http://prowlarr.default.svc.cluster.local/2/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // avz
     `http://prowlarr.default.svc.cluster.local/8/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // ptp
     `http://prowlarr.default.svc.cluster.local/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // btn
+    `http://prowlarr.default.svc.cluster.local/20/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tl
     `http://prowlarr.default.svc.cluster.local/26/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mtv
+    `http://prowlarr.default.svc.cluster.local/40/api?apikey=$${process.env.PROWLARR_API_KEY}`, // uhdb
     `http://prowlarr.default.svc.cluster.local/45/api?apikey=$${process.env.PROWLARR_API_KEY}`, // bhd
+    `http://prowlarr.default.svc.cluster.local/47/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ar
     `http://prowlarr.default.svc.cluster.local/48/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ant
     `http://prowlarr.default.svc.cluster.local/49/api?apikey=$${process.env.PROWLARR_API_KEY}`, // athr
+    `http://prowlarr.default.svc.cluster.local/84/api?apikey=$${process.env.PROWLARR_API_KEY}`, // nbl
   ],
   port: process.env.CROSSSEED_PORT || 80,
   apiAuth: false,
