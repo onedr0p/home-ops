bff73ac5d4041a175a88522bc929f60aac56d2a1
Devin Buhl <devin@buhl.casa>
2024-03-20 14:46:04 -0400

    fix: update talos upgrade script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .taskfiles/Talos/scripts/upgrade.sh
+++ .taskfiles/Talos/scripts/upgrade.sh
@@ -5,40 +5,51 @@ NODE="${2}"
 TALOS_STANZA="${3}"
 ROLLOUT="${4:-false}"
 
-echo "Waiting for all jobs to complete before upgrading Talos ..."
+FROM_VERSION=$(kubectl --context "${CLUSTER}" get node "${NODE}" --output jsonpath='{.metadata.labels.feature\.node\.kubernetes\.io/system-os_release\.VERSION_ID}')
+TO_VERSION=${TALOS_STANZA##*:}
+
+echo "Checking if Talos needs to be upgraded on node '${NODE}' in cluster '${CLUSTER}' ..."
+if [ "${FROM_VERSION}" == "${TO_VERSION}" ]; then
+    echo "Talos is already up to date on version '${FROM_VERSION}', skipping upgrade ..."
+    exit 0
+fi
+
+echo "Waiting for all jobs to complete before upgrading Talos on node '${NODE}' in cluster '${CLUSTER}' ..."
 until kubectl --context "${CLUSTER}" wait --timeout=5m \
     --for=condition=Complete jobs --all --all-namespaces;
 do
-    echo "Waiting for jobs to complete ..."
+    echo "Waiting for all jobs to complete before upgrading Talos on node '${NODE}' in cluster '${CLUSTER}' ..."
     sleep 10
 done
 
 if [ "${ROLLOUT}" != "true" ]; then
-    echo "Suspending Flux Kustomizations ..."
+    echo "Suspending Flux Kustomizations in cluster '${CLUSTER}' ..."
     flux --context "${CLUSTER}" suspend kustomization --all
-    echo "Setting CNPG maintenance mode ..."
+    echo "Setting CNPG maintenance mode in cluster '${CLUSTER}' ..."
     kubectl cnpg --context "${CLUSTER}" maintenance set --reusePVC --all-namespaces
 fi
 
-echo "Upgrading Talos on node ${NODE} in cluster ${CLUSTER} ..."
+echo "Upgrading Talos on node '${NODE}' in cluster '${CLUSTER}' to ${TO_VERSION}..."
 talosctl --context "${CLUSTER}" --nodes "${NODE}" upgrade \
     --image="factory.talos.dev/installer/${TALOS_STANZA}" \
         --wait=true --timeout=10m --preserve=true
 
-echo "Waiting for Talos to be healthy ..."
+echo "Waiting for Talos to be healthy on node '${NODE}' in cluster '${CLUSTER}' ..."
 talosctl --context "${CLUSTER}" --nodes "${NODE}" health \
     --wait-timeout=10m --server=false
 
-echo "Waiting for Ceph health to be OK ..."
+echo "Waiting for Ceph health to be OK on node '${NODE}' in cluster '${CLUSTER}' ..."
 until kubectl --context "${CLUSTER}" wait --timeout=5m \
     --for=jsonpath=.status.ceph.health=HEALTH_OK cephcluster \
         --all --all-namespaces;
 do
-    echo "Waiting for Ceph health to be OK ..."
+    echo "Waiting for Ceph health to be OK on node '${NODE}' in cluster '${CLUSTER}' ..."
     sleep 10
 done
 
 if [ "${ROLLOUT}" != "true" ]; then
-    echo "Unsetting CNPG maintenance mode ..."
+    echo "Resuming Flux Kustomizations in cluster '${CLUSTER}' ..."
     flux --context "${CLUSTER}" resume kustomization --all
+    echo "Unsetting CNPG maintenance mode in cluster '${CLUSTER}' ..."
+    kubectl cnpg --context "${CLUSTER}" maintenance unset --reusePVC --all-namespaces
 fi
