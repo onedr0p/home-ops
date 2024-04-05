6069bcfe6c379c2d6a87699f737c6291bb75ff24
Devin Buhl <devin@buhl.casa>
2024-04-05 07:28:46 -0400

    fix(cross-seed): torznab is not needed
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/main/apps/default/cross-seed/app/resources/config.js
+++ kubernetes/main/apps/default/cross-seed/app/resources/config.js
@@ -2,19 +2,7 @@
 module.exports = {
   delay: 20,
   qbittorrentUrl: "http://qbittorrent.default.svc.cluster.local",
-  torznab: [
-    `http://prowlarr.default.svc.cluster.local/2/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // avz
-    `http://prowlarr.default.svc.cluster.local/8/api?apikey=$${process.env.PROWLARR_API_KEY}`,  // ptp
-    `http://prowlarr.default.svc.cluster.local/11/api?apikey=$${process.env.PROWLARR_API_KEY}`, // btn
-    `http://prowlarr.default.svc.cluster.local/20/api?apikey=$${process.env.PROWLARR_API_KEY}`, // tl
-    `http://prowlarr.default.svc.cluster.local/26/api?apikey=$${process.env.PROWLARR_API_KEY}`, // mtv
-    `http://prowlarr.default.svc.cluster.local/40/api?apikey=$${process.env.PROWLARR_API_KEY}`, // uhdb
-    `http://prowlarr.default.svc.cluster.local/45/api?apikey=$${process.env.PROWLARR_API_KEY}`, // bhd
-    `http://prowlarr.default.svc.cluster.local/47/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ar
-    `http://prowlarr.default.svc.cluster.local/48/api?apikey=$${process.env.PROWLARR_API_KEY}`, // ant
-    `http://prowlarr.default.svc.cluster.local/49/api?apikey=$${process.env.PROWLARR_API_KEY}`, // athr
-    `http://prowlarr.default.svc.cluster.local/84/api?apikey=$${process.env.PROWLARR_API_KEY}`, // nbl
-  ],
+  torznab: [`http://prowlarr.default.svc.cluster.local/1/api?apikey=$${process.env.PROWLARR_API_KEY}`],
   port: process.env.CROSSSEED_PORT || 80,
   apiAuth: false,
   action: "inject",
