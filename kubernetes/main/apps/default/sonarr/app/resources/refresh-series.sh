dc78c1d131c5a82039bbacd0364203ebd842d92c
Devin Buhl <devin@buhl.casa>
2024-08-09 16:48:18 -0400

    fix: tba sonarr problem solved question mark question mark
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/main/apps/default/sonarr/app/resources/refresh-series.sh
+++ kubernetes/main/apps/default/sonarr/app/resources/refresh-series.sh
@@ -5,7 +5,7 @@ if [[ "${sonarr_eventtype:-}" == "Grab" ]]; then
     tba_count=$(curl -fsSL \
         --header "X-Api-Key: ${SONARR__AUTH__APIKEY}" \
         "http://localhost:${SONARR__SERVER__PORT}/api/v3/episode?seriesId=${sonarr_series_id}" \
-            | jq --raw-output '.[] | select((.title == "TBA") or (.title == "TBD"))' | jq --slurp 'length')
+            | jq --raw-output '[.[] | select((.title == "TBA") or (.title == "TBD"))] | length')
 
     if (( tba_count > 0 )); then
         echo "INFO: Refreshing series ${sonarr_series_id} due to TBA/TBD episodes found"
