84f8caa32cd3673be3d18e9a60e9ed0182084067
Devin Buhl <devin@buhl.casa>
2024-08-20 10:19:03 -0400

    fix: qb update completed script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/main/apps/default/qbittorrent/app/resources/completed.sh
+++ kubernetes/main/apps/default/qbittorrent/app/resources/completed.sh
@@ -5,21 +5,18 @@
 
 /bin/chmod -R 750 "$1"
 
-if [[ "$2" == *"force-cross-seed"* ]]; then
-    printf "Searching cross-seed for '%s' with tags '%s'\n" "$1" "$2"
-    /usr/bin/curl \
-        --silent \
-        --connect-timeout 5 \
-        --max-time 10 \
-        --retry 5 \
-        --retry-delay 0 \
-        --retry-max-time 40 \
-        --request POST \
-        --data-urlencode "path=$1" \
-        --header "X-Api-Key: $3" \
-        http://cross-seed.default.svc.cluster.local/api/webhook
-else
-    printf "Skipping cross-seed check for '%s' with tags '%s'\n" "$1" "$2"
-fi
+printf "Searching cross-seed for '%s' with tags '%s'\n" "$1" "$2"
+
+/usr/bin/curl \
+    --silent \
+    --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    --request POST \
+    --data-urlencode "path=$1" \
+    --header "X-Api-Key: $3" \
+    http://cross-seed.default.svc.cluster.local/api/webhook
 
 exit 0
