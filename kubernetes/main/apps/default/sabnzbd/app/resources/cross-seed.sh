d2a6f2f0044efad87a8e5122e8a29823c6cd35bf
Devin Buhl <devin@buhl.casa>
2024-08-22 20:40:02 -0400

    fix: update sab cross-seed script
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/main/apps/default/sabnzbd/app/resources/cross-seed.sh
+++ kubernetes/main/apps/default/sabnzbd/app/resources/cross-seed.sh
@@ -2,34 +2,30 @@
 
 export CROSS_SEED_HOST=${CROSS_SEED_HOST:-cross-seed.default.svc.cluster.local}
 export CROSS_SEED_PORT=${CROSS_SEED_PORT:-80}
+export CROSS_SEED_API_KEY=${CROSS_SEED_PORT:-unset}
+export CROSS_SEED_SLEEP_INTERVAL=${SLEEP_INTERVAL:-30}
 
 SEARCH_PATH=$1
 
-log_message() {
-    local log_type="$1"
-    local message="$2"
-    local timestamp
-    timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
-    echo "${timestamp} [${log_type}] ${message}"
-}
+# Update permissions on the search path
+chmod -R 750 "${SEARCH_PATH}"
 
-cross_seed_request() {
-    local endpoint="$1"
-    local data="$2"
-    local headers=(-X POST "http://${CROSS_SEED_HOST}:${CROSS_SEED_PORT}/api/${endpoint}" --data-urlencode "${data}")
-    if [[ -n "${CROSS_SEED_API_KEY}" ]]; then
-        headers+=(-H "X-Api-Key: ${CROSS_SEED_API_KEY}")
-    fi
-    response=$(curl --silent --output /dev/null --write-out "%{http_code}" "${headers[@]}")
-    echo "${response}"
-}
+# Search for cross-seed
+response=$(curl \
+    --silent \
+    --output /dev/null \
+    --write-out "%{http_code}" \
+    --request POST \
+    --data-urlencode "path=${SEARCH_PATH}" \
+    --header "X-Api-Key: ${CROSS_SEED_API_KEY}" \
+    "http://${CROSS_SEED_HOST}:${CROSS_SEED_PORT}/api/webhook"
+)
 
-cross_seed_resp=$(cross_seed_request "webhook" "path=${SEARCH_PATH}")
-
-if [[ "${cross_seed_resp}" == "204" ]]; then
-    log_message "INFO" "Process completed successfully."
-    sleep 30
-else
-    log_message "ERROR" "Process failed with API response: ${cross_seed_resp}"
+if [[ "${response}" != "204" ]]; then
+    printf "Failed to search cross-seed for '%s'\n" "${SEARCH_PATH}"
     exit 1
 fi
+
+printf "Successfully searched cross-seed for '%s'\n" "${SEARCH_PATH}"
+
+sleep "${SLEEP_INTERVAL}"
