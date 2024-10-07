7dbef4e5889392c27ef85b7e6b4e3341418e240f
Devin Buhl <devin@buhl.casa>
2024-10-07 08:21:19 -0400

    fix: update volsync Taskfiles
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- .taskfiles/volsync/resources/wait-for-job.sh
+++ .taskfiles/volsync/resources/wait-for-job.sh
@@ -4,9 +4,10 @@ JOB=$1
 NAMESPACE="${2:-default}"
 
 [[ -z "${JOB}" ]] && echo "Job name not specified" && exit 1
+
 while true; do
-    STATUS="$(kubectl --namespace "${NAMESPACE}" get pod --selector="job-name=${JOB}" --output="jsonpath='{.items[*].status.phase}'")"
-    if [ "${STATUS}" == "Pending" ]; then
+    STATUS="$(kubectl --namespace "${NAMESPACE}" get pod -l job-name="${JOB}" -o jsonpath='{.items[*].status.phase}')"
+    if [[ "${STATUS}" == "Pending" ]]; then
         break
     fi
     sleep 1
