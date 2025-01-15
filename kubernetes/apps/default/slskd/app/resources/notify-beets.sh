b59005015d51060e2fa1765d30bfb50ad8f4858b
Devin Buhl <devin@buhl.casa>
2025-01-15 08:33:07 -0500

    feat: add the beets/slskd integration
    Signed-off-by: Devin Buhl <devin@buhl.casa>

---
+++ kubernetes/apps/default/slskd/app/resources/notify-beets.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
+
+wget -q -O/dev/null \
+     --post-data "name=${name}&path=${name}" \
+     --header="X-API-KEY: ${BETANIN_API_KEY}" \
+     --header="User-Agent: notify-beats.sh" \
+      "http://${BEETS_HOST}/api/torrents"
