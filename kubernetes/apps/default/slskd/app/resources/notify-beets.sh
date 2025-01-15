1a791ff60302040883c14a10eeb9e07c39170660
Devin Buhl <devin@buhl.casa>
2025-01-15 10:44:02 -0500

    fix(slskd): more debuggin
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/slskd/app/resources/notify-beets.sh
+++ kubernetes/apps/default/slskd/app/resources/notify-beets.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
 
 wget -q -O/dev/null \
-     --post-data "name=${name}&path=${name}" \
-     --header="X-API-KEY: ${BETANIN_API_KEY}" \
+     --post-data "name=$name&path=$name" \
+     --header="X-API-KEY: $BETANIN_API_KEY" \
      --header="User-Agent: notify-beats.sh" \
-      "http://${BEETS_HOST}/api/torrents"
+      "http://$BEETS_HOST/api/torrents"
