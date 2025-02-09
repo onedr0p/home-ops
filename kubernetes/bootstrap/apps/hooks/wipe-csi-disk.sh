c4e6bd43cbf112d0953779adbbe9379830428d99
Devin Buhl <devin@buhl.casa>
2025-02-09 12:59:30 -0500

    feat(helmfile): use global hooks for prom crds and wipe rook disk
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/bootstrap/apps/hooks/wipe-csi-disk.sh
+++ kubernetes/bootstrap/apps/hooks/wipe-csi-disk.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 is_node_ready() {
-    local node_ip=$1
+    local node=$1
     local ready_status
-    ready_status=$(talosctl get machinestatus --output json | jq --raw-output --slurp ".[] | select(.node == \"$node_ip\") | .spec.status.ready")
+    ready_status=$(talosctl get nodestatus --output json \
+        | jq --raw-output --slurp ".[] | select(.node == \"$node\") | .spec.nodeReady")
     if [ "${ready_status}" == "true" ]; then
         return 0
     else
@@ -11,24 +12,41 @@ is_node_ready() {
     fi
 }
 
-wipe_csi_disk() {
-    local disks
-    disks=$(talosctl get disks --output json | jq --slurp --compact-output '
-        map(select(.spec.model == env.CSI_DISK))
-            | group_by(.node)
-            | map({ (.[0].node): (map(.metadata.id) | join(" ")) })
-            | add
-    ')
+get_nodes() {
+    talosctl config info --output json \
+        | jq --raw-output '.nodes | .[]'
+}
 
-    jq --raw-output 'to_entries[] | "\(.key) \(.value)"' <<< "${disks}" | while read -r ip disk; do
-        if ! is_node_ready "$ip"; then
-            talosctl --nodes "$ip" get disk "${disk}"
+get_disk() {
+    local node=$1
+    talosctl --nodes "${node}" get disks --output json \
+        | jq --raw-output --slurp '. | map(select(.spec.model == env.CSI_DISK) | .metadata.id) | join(" ")'
+}
+
+wipe_disk() {
+    local node=$1
+    local disk=$2
+
+    if [[ -n "${disk}" ]]; then
+        if ! is_node_ready "${node}"; then
+            printf "Wiping disk %s on %s\n" "${disk}" "${node}"
+            talosctl --nodes "${node}" get disk "${disk}"
+        else
+            printf "Node %s is already ready\n" "${node}"
         fi
-    done
+    else
+        printf "No matching disk found on %s\n" "${node}"
+    fi
 }
 
 main() {
-    wipe_csi_disk
+    local node disk
+
+    for node in $(get_nodes); do
+        printf "Getting disk id for %s\n" "${node}"
+        disk=$(get_disk "${node}")
+        wipe_disk "${node}" "${disk}"
+    done
 }
 
 main
