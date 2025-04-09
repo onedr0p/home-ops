a96f040a6f54f9d697ce5554a90e7f646b2c740a
Devin Buhl <devin@buhl.casa>
2025-04-09 16:50:44 -0400

    chore: update slskd
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/slskd/app/resources/beets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-name="$(jq --raw-output '.localDirectoryName' <<< $1)"
-
-wget -q -O/dev/null \
-     --post-data "name=${name}&path=${name}" \
-     --header="X-API-KEY: ${BETANIN_API_KEY}" \
-     --header="User-Agent: slskd/0.0" \
-      "http://${BEETS_HOST}/api/torrents"
