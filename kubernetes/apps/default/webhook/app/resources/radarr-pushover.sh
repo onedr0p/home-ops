7362350241442cf50338cee8cabf16e3e24abc35
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-15 22:19:07 -0400

    chore: update webhook config (#9124)

--- kubernetes/apps/default/webhook/app/resources/radarr-pushover.sh
+++ kubernetes/apps/default/webhook/app/resources/radarr-pushover.sh
@@ -4,21 +4,21 @@ set -Eeuo pipefail
 function notify() {
     if [[ "${RADARR_EVENT_TYPE}" == "Test" ]]; then
         printf -v PUSHOVER_TITLE "Test Notification"
-        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification from <b>%s</b>" "${RADARR_INSTANCE_NAME}"
+        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification"
         printf -v PUSHOVER_URL "%s" "${RADARR_APPLICATION_URL}"
-        printf -v PUSHOVER_URL_TITLE "Open %s" "${RADARR_INSTANCE_NAME}"
-        printf -v PUSHOVER_PRIORITY "%s" "low"
+        printf -v PUSHOVER_URL_TITLE "View Movies"
+        printf -v PUSHOVER_PRIORITY "low"
     elif [[ "${RADARR_EVENT_TYPE}" == "ManualInteractionRequired" ]]; then
-        printf -v PUSHOVER_TITLE "%s Import Requires Manual Interaction" "Movie"
+        printf -v PUSHOVER_TITLE "Movie Requires Manual Interaction"
         printf -v PUSHOVER_MESSAGE "<b>%s (%s)</b><small>\n<b>Client:</b> %s</small>" \
             "${RADARR_MOVIE_TITLE}" \
             "${RADARR_MOVIE_YEAR}" \
             "${RADARR_DOWNLOAD_CLIENT}"
         printf -v PUSHOVER_URL "%s/activity/queue" "${RADARR_APPLICATION_URL}"
-        printf -v PUSHOVER_URL_TITLE "View queue in %s" "${RADARR_INSTANCE_NAME}"
-        printf -v PUSHOVER_PRIORITY "%s" "high"
+        printf -v PUSHOVER_URL_TITLE "View Queue"
+        printf -v PUSHOVER_PRIORITY "high"
     elif [[ "${RADARR_EVENT_TYPE}" == "Download" ]]; then
-        printf -v PUSHOVER_TITLE "Movie %s" "Imported"
+        printf -v PUSHOVER_TITLE "Movie Added"
         printf -v PUSHOVER_MESSAGE "<b>%s (%s)</b><small>\n%s</small><small>\n\n<b>Client:</b> %s</small>" \
             "${RADARR_MOVIE_TITLE}" \
             "${RADARR_MOVIE_YEAR}" \
@@ -27,8 +27,8 @@ function notify() {
         printf -v PUSHOVER_URL "%s/movie/%s" \
             "${RADARR_APPLICATION_URL}" \
             "${RADARR_MOVIE_TMDB_ID}"
-        printf -v PUSHOVER_URL_TITLE "View movie in %s" "${RADARR_INSTANCE_NAME}"
-        printf -v PUSHOVER_PRIORITY "%s" "low"
+        printf -v PUSHOVER_URL_TITLE "View Movie"
+        printf -v PUSHOVER_PRIORITY "low"
     fi
 
     apprise -vv --title "${PUSHOVER_TITLE}" --body "${PUSHOVER_MESSAGE}" --input-format html \
