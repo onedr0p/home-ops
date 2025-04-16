68b334239a8824e0ca2367d2d029b75cd5c3b98a
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-15 22:15:05 -0400

    Update radarr-pushover.sh

--- kubernetes/apps/default/webhook/app/resources/radarr-pushover.sh
+++ kubernetes/apps/default/webhook/app/resources/radarr-pushover.sh
@@ -4,9 +4,9 @@ set -Eeuo pipefail
 function notify() {
     if [[ "${RADARR_EVENT_TYPE}" == "Test" ]]; then
         printf -v PUSHOVER_TITLE "Test Notification"
-        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification from <b>%s</b>" "${RADARR_INSTANCE_NAME}"
+        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification"
         printf -v PUSHOVER_URL "%s" "${RADARR_APPLICATION_URL}"
-        printf -v PUSHOVER_URL_TITLE "Open %s" "${RADARR_INSTANCE_NAME}"
+        printf -v PUSHOVER_URL_TITLE "View Movies"
         printf -v PUSHOVER_PRIORITY "low"
     elif [[ "${RADARR_EVENT_TYPE}" == "ManualInteractionRequired" ]]; then
         printf -v PUSHOVER_TITLE "Movie Requires Manual Interaction"
@@ -15,7 +15,7 @@ function notify() {
             "${RADARR_MOVIE_YEAR}" \
             "${RADARR_DOWNLOAD_CLIENT}"
         printf -v PUSHOVER_URL "%s/activity/queue" "${RADARR_APPLICATION_URL}"
-        printf -v PUSHOVER_URL_TITLE "View queue in %s" "${RADARR_INSTANCE_NAME}"
+        printf -v PUSHOVER_URL_TITLE "View Queue"
         printf -v PUSHOVER_PRIORITY "high"
     elif [[ "${RADARR_EVENT_TYPE}" == "Download" ]]; then
         printf -v PUSHOVER_TITLE "Movie Added"
@@ -27,7 +27,7 @@ function notify() {
         printf -v PUSHOVER_URL "%s/movie/%s" \
             "${RADARR_APPLICATION_URL}" \
             "${RADARR_MOVIE_TMDB_ID}"
-        printf -v PUSHOVER_URL_TITLE "View movie in %s" "${RADARR_INSTANCE_NAME}"
+        printf -v PUSHOVER_URL_TITLE "View Movie"
         printf -v PUSHOVER_PRIORITY "low"
     fi
 
