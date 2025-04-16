a5d1c703e310cfc0b5569eed7617ec49d3df2619
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-15 22:12:07 -0400

    Update sonarr-pushover.sh

--- kubernetes/apps/default/webhook/app/resources/sonarr-pushover.sh
+++ kubernetes/apps/default/webhook/app/resources/sonarr-pushover.sh
@@ -4,21 +4,20 @@ set -Eeuo pipefail
 function notify() {
     if [[ "${SONARR_EVENT_TYPE}" == "Test" ]]; then
         printf -v PUSHOVER_TITLE "Test Notification"
-        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification from <b>%s</b>" "${SONARR_INSTANCE_NAME}"
+        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification"
         printf -v PUSHOVER_URL "%s" "${SONARR_APPLICATION_URL}"
-        printf -v PUSHOVER_URL_TITLE "Open %s" "${SONARR_INSTANCE_NAME}"
-        printf -v PUSHOVER_PRIORITY "%s" "low"
+        printf -v PUSHOVER_URL_TITLE "View Series"
+        printf -v PUSHOVER_PRIORITY "low"
     elif [[ "${SONARR_EVENT_TYPE}" == "ManualInteractionRequired" ]]; then
-        printf -v PUSHOVER_TITLE "%s Import Requires Manual Interaction" "Episode"
-        printf -v PUSHOVER_MESSAGE "<b>%s (%s)</b><small>\n<b>Client:</b> %s</small>" \
+        printf -v PUSHOVER_TITLE "Episode Requires Manual Interaction"
+        printf -v PUSHOVER_MESSAGE "<b>%s</b><small>\n<b>Client:</b> %s</small>" \
             "${SONARR_SERIES_TITLE}" \
-            "${SONARR_SERIES_YEAR}" \
             "${SONARR_DOWNLOAD_CLIENT}"
         printf -v PUSHOVER_URL "%s/activity/queue" "${SONARR_APPLICATION_URL}"
-        printf -v PUSHOVER_URL_TITLE "View queue in %s" "${SONARR_INSTANCE_NAME}"
-        printf -v PUSHOVER_PRIORITY "%s" "high"
+        printf -v PUSHOVER_URL_TITLE "View Queue"
+        printf -v PUSHOVER_PRIORITY "high"
     elif [[ "${SONARR_EVENT_TYPE}" == "Download" ]]; then
-        printf -v PUSHOVER_TITLE "Episode %s" "Imported"
+        printf -v PUSHOVER_TITLE "Episode Added"
         printf -v PUSHOVER_MESSAGE "<b>%s (S%02dE%02d)</b><small>\n%s</small><small>\n\n<b>Client:</b> %s</small><small>" \
             "${SONARR_SERIES_TITLE}" \
             "${SONARR_EPISODE_SEASON_NUMBER}" \
@@ -28,8 +27,8 @@ function notify() {
         printf -v PUSHOVER_URL "%s/series/%s" \
             "${SONARR_APPLICATION_URL}" \
             "${SONARR_SERIES_TITLE_SLUG}"
-        printf -v PUSHOVER_URL_TITLE "View series in %s" "${SONARR_INSTANCE_NAME}"
-        printf -v PUSHOVER_PRIORITY "%s" "low"
+        printf -v PUSHOVER_URL_TITLE "View Series"
+        printf -v PUSHOVER_PRIORITY "low"
     fi
 
     apprise -vv --title "${PUSHOVER_TITLE}" --body "${PUSHOVER_MESSAGE}" --input-format html \
