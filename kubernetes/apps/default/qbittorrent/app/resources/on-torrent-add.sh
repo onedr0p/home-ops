22fc86247f51b95b4ab84a065d6cf1bc75daa7b9
Devin Buhl <devin@buhl.casa>
2025-07-22 10:47:53 -0400

    chore: make qbt quiet
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/resources/on-torrent-add.sh
+++ kubernetes/apps/default/qbittorrent/app/resources/on-torrent-add.sh
@@ -4,7 +4,8 @@ set -Eeuo pipefail
 INFOHASH="${1:?}"
 
 # Ref: https://github.com/buroa/qbr
+# TODO: Bring binary into the container via an OCI volume mount
 /config/qbr --hash "${INFOHASH}"
 
 # Ref: https://github.com/cross-seed/cross-seed/issues/945
-qbt tag delete "cross-seed"
+qbt tag delete "cross-seed" --quiet
