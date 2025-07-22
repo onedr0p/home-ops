914332cdc9aa7630c46c38a8ecf4d56b6710d305
Devin Buhl <devin@buhl.casa>
2025-07-22 18:25:51 -0400

    chore: qbr is now qbrr
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- kubernetes/apps/default/qbittorrent/app/resources/on-torrent-add.sh
+++ kubernetes/apps/default/qbittorrent/app/resources/on-torrent-add.sh
@@ -3,9 +3,9 @@ set -Eeuo pipefail
 
 INFOHASH="${1:?}"
 
-# Ref: https://github.com/buroa/qbr
+# Ref: https://github.com/buroa/qbrr
 # TODO: Bring binary into the container via an OCI volume mount
-/config/qbr --hash "${INFOHASH}"
+/config/qbrr --hash "${INFOHASH}"
 
 # Ref: https://github.com/cross-seed/cross-seed/issues/945
 qbt tag delete "cross-seed" --quiet
